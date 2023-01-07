5f54e45bb4719ab6dab34bae7d351200cfb3c23c
mal1on <lyubo.malinov@gmail.com>
2023-01-07 21:57:25 +0200

    all three checks successful

--- Blizzard/Pattern Recognition.py
+++ Blizzard/Pattern Recognition.py
@@ -4,11 +4,13 @@ def checkio(pattern, image):
         for h_ind in range(len(image[0]) - len(pattern[0]) + 1):
             check = True
             if image[v_ind][h_ind:h_ind + len(pattern[0])] != pattern[0]:
-                check = False
-            for vert in range(v_ind + 1, len(pattern)):
-                if image[vert][h_ind:h_ind + len(pattern[0])] != pattern[vert]:
-                    check = False
-            print(v_ind, image[v_ind][h_ind:h_ind + len(pattern[0])], check)                                
+                check = False    
+            #print('v_ind:', v_ind, image[v_ind][h_ind:h_ind + len(pattern[0])], 'check:', check)    
+            for vert in range(1, len(pattern)):
+                #if check == True:
+                    #print(image[v_ind + vert][h_ind:h_ind + len(pattern[0])])
+                if image[v_ind + vert][h_ind:h_ind + len(pattern[0])] != pattern[vert]:
+                    check = False                                
             if check:
                 for c_ind in range(len(pattern[0])):
                     if image[v_ind][h_ind + c_ind] == 0:
@@ -42,4 +44,26 @@ checkio([[1, 1], [1, 1]],
          [1, 1, 1],
          [1, 1, 1]]) == [[3, 3, 1],
                          [3, 3, 1],
-                         [1, 1, 1]]                                  
+                         [1, 1, 1]]
+
+
+checkio([[0, 1, 0], [1, 1, 1]],
+                   [[0, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+                    [0, 1, 1, 1, 0, 0, 0, 1, 1, 1],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+                    [0, 1, 0, 0, 1, 1, 1, 0, 1, 0],
+                    [1, 1, 1, 0, 0, 0, 0, 0, 1, 1],
+                    [0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                    [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+                    [0, 1, 1, 0, 0, 0, 1, 1, 1, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]) == [[0, 2, 3, 2, 0, 0, 0, 2, 3, 2],
+                                                         [0, 3, 3, 3, 0, 0, 0, 3, 3, 3],
+                                                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                                         [0, 0, 0, 0, 2, 3, 2, 0, 0, 0],
+                                                         [2, 3, 2, 0, 3, 3, 3, 0, 1, 0],
+                                                         [3, 3, 3, 0, 0, 0, 0, 0, 1, 1],
+                                                         [0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                                                         [0, 0, 1, 0, 0, 0, 2, 3, 2, 0],
+                                                         [0, 1, 1, 0, 0, 0, 3, 3, 3, 0],
+                                                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]                                                           
