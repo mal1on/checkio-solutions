6355b8066d746279886a8f2943993cddd1ce27c3
mal1on <lyubo.malinov@gmail.com>
2023-01-07 22:20:33 +0200

    Blizzard - Pattern Recognition complete

--- Blizzard/Pattern Recognition.py
+++ Blizzard/Pattern Recognition.py
@@ -4,51 +4,44 @@ def checkio(pattern, image):
         for h_ind in range(len(image[0]) - len(pattern[0]) + 1):
             check = True
             if image[v_ind][h_ind:h_ind + len(pattern[0])] != pattern[0]:
-                check = False    
-            #print('v_ind:', v_ind, image[v_ind][h_ind:h_ind + len(pattern[0])], 'check:', check)    
+                check = False
             for vert in range(1, len(pattern)):
-                #if check == True:
-                    #print(image[v_ind + vert][h_ind:h_ind + len(pattern[0])])
                 if image[v_ind + vert][h_ind:h_ind + len(pattern[0])] != pattern[vert]:
-                    check = False                                
+                    check = False
             if check:
                 for c_ind in range(len(pattern[0])):
                     if image[v_ind][h_ind + c_ind] == 0:
                         image[v_ind][h_ind + c_ind] = 2
                     else:
                         image[v_ind][h_ind + c_ind] = 3
-                    for vert in range(1, len(pattern)):    
+                    for vert in range(1, len(pattern)):
                         if image[v_ind + vert][h_ind + c_ind] == 0:
                             image[v_ind + vert][h_ind + c_ind] = 2
                         else:
-                            image[v_ind + vert][h_ind + c_ind] = 3    
-    print(image)                                         
+                            image[v_ind + vert][h_ind + c_ind] = 3
+
+    return image
 
 
-checkio([[1,0,0],[0,1,0],[0,0,1]],[[1,0,0,0,1,0,1,0,1],[0,1,0,0,0,1,0,1,0],[0,0,1,0,0,0,1,0,1],[1,0,0,1,0,0,0,1,0],[0,0,0,0,1,0,0,0,1],[0,1,0,0,0,1,0,0,0],[0,1,0,0,0,0,1,0,1]])
-
-
-checkio([[1, 0], [1, 1]],
-        [[0, 1, 0, 1, 0],
-         [0, 1, 1, 0, 0],
-         [1, 0, 1, 1, 0],
-         [1, 1, 0, 1, 1],
-         [0, 1, 1, 0, 0]]) == [[0, 3, 2, 1, 0],
-                               [0, 3, 3, 0, 0],
-                               [3, 2, 1, 3, 2],
-                               [3, 3, 0, 3, 3],
-                               [0, 1, 1, 0, 0]]
-
-
-checkio([[1, 1], [1, 1]],
-        [[1, 1, 1],
-         [1, 1, 1],
-         [1, 1, 1]]) == [[3, 3, 1],
-                         [3, 3, 1],
-                         [1, 1, 1]]
-
-
-checkio([[0, 1, 0], [1, 1, 1]],
+# These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio([[1, 0], [1, 1]],
+                   [[0, 1, 0, 1, 0],
+                    [0, 1, 1, 0, 0],
+                    [1, 0, 1, 1, 0],
+                    [1, 1, 0, 1, 1],
+                    [0, 1, 1, 0, 0]]) == [[0, 3, 2, 1, 0],
+                                          [0, 3, 3, 0, 0],
+                                          [3, 2, 1, 3, 2],
+                                          [3, 3, 0, 3, 3],
+                                          [0, 1, 1, 0, 0]]
+    assert checkio([[1, 1], [1, 1]],
+                   [[1, 1, 1],
+                    [1, 1, 1],
+                    [1, 1, 1]]) == [[3, 3, 1],
+                                    [3, 3, 1],
+                                    [1, 1, 1]]
+    assert checkio([[0, 1, 0], [1, 1, 1]],
                    [[0, 0, 1, 0, 0, 0, 0, 0, 1, 0],
                     [0, 1, 1, 1, 0, 0, 0, 1, 1, 1],
                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -59,12 +52,20 @@ checkio([[0, 1, 0], [1, 1, 1]],
                     [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
                     [0, 1, 1, 0, 0, 0, 1, 1, 1, 0],
                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]) == [[0, 2, 3, 2, 0, 0, 0, 2, 3, 2],
-                                                         [0, 3, 3, 3, 0, 0, 0, 3, 3, 3],
-                                                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                                                         [0, 0, 0, 0, 2, 3, 2, 0, 0, 0],
-                                                         [2, 3, 2, 0, 3, 3, 3, 0, 1, 0],
-                                                         [3, 3, 3, 0, 0, 0, 0, 0, 1, 1],
-                                                         [0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
-                                                         [0, 0, 1, 0, 0, 0, 2, 3, 2, 0],
-                                                         [0, 1, 1, 0, 0, 0, 3, 3, 3, 0],
-                                                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]                                                           
+                                                         [0, 3, 3, 3, 0, 0,
+                                                             0, 3, 3, 3],
+                                                         [0, 0, 0, 0, 0, 0,
+                                                             0, 0, 0, 0],
+                                                         [0, 0, 0, 0, 2, 3,
+                                                             2, 0, 0, 0],
+                                                         [2, 3, 2, 0, 3, 3,
+                                                             3, 0, 1, 0],
+                                                         [3, 3, 3, 0, 0, 0,
+                                                             0, 0, 1, 1],
+                                                         [0, 0, 0, 1, 1, 1,
+                                                             0, 0, 0, 0],
+                                                         [0, 0, 1, 0, 0, 0,
+                                                             2, 3, 2, 0],
+                                                         [0, 1, 1, 0, 0, 0,
+                                                             3, 3, 3, 0],
+                                                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
