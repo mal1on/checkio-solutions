cced92a81656f538f25cde4a3d6687111cec4be1
mal1on <lyubo.malinov@gmail.com>
2023-01-08 17:21:18 +0200

    Ïce Base - Similar Triangles - Asserts complete

--- Ice Base/Similar Triangles.py
+++ Ice Base/Similar Triangles.py
@@ -7,7 +7,8 @@ def similar_triangles(coords_1: Coords, coords_2: Coords) -> bool:
 
     def angle(a, b, c):
 
-        ang = degrees(atan2(c[1] - b[1], c[0] - b[0]) - atan2(a[1] - b[1], a[0] - b[0]))
+        ang = degrees(atan2(c[1] - b[1], c[0] - b[0]) -
+                      atan2(a[1] - b[1], a[0] - b[0]))
 
         return ang + 360 if ang < 0 else ang
 
@@ -15,22 +16,34 @@ def similar_triangles(coords_1: Coords, coords_2: Coords) -> bool:
     angles_2 = set()
 
     a, b, c = coords_1
-    angles_1.add(angle(a, b, c))
-    angles_1.add(angle(c, a, b))
+    angles_1.add(round(angle(a, b, c)))
+    angles_1.add(round(angle(c, a, b)))
     angles_1.add(180 - sum(angles_1))
 
     a, b, c = coords_2
-    angles_2.add(angle(a, b, c))
-    angles_2.add(angle(c, a, b))
+    angles_2.add(round(angle(a, b, c)))
+    angles_2.add(round(angle(c, a, b)))
     angles_2.add(180 - sum(angles_2))
 
-    print(angles_1 == angles_2) 
+    return angles_1 == angles_2
 
 
+if __name__ == '__main__':
+    print("Example:")
+    print(similar_triangles(
+        [(0, 0), (1, 2), (2, 0)], [(3, 0), (4, 2), (5, 0)]))
 
-
-
-
-similar_triangles([(0, 0), (1, 2), (2, 0)], [(3, 0), (4, 2), (5, 0)]) is True
-similar_triangles([(0, 0), (1, 2), (2, 0)], [(3, 0), (4, 3), (5, 0)]) is False
-similar_triangles([(1, 0), (1, 2), (2, 0)], [(3, 0), (5, 4), (5, 0)]) is True
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert similar_triangles([(0, 0), (1, 2), (2, 0)], [
+                             (3, 0), (4, 2), (5, 0)]) is True, 'basic'
+    assert similar_triangles([(0, 0), (1, 2), (2, 0)], [(
+        3, 0), (4, 3), (5, 0)]) is False, 'different #1'
+    assert similar_triangles([(0, 0), (1, 2), (2, 0)], [
+                             (2, 0), (4, 4), (6, 0)]) is True, 'scaling'
+    assert similar_triangles([(0, 0), (0, 3), (2, 0)], [
+                             (3, 0), (5, 3), (5, 0)]) is True, 'reflection'
+    assert similar_triangles([(1, 0), (1, 2), (2, 0)], [(
+        3, 0), (5, 4), (5, 0)]) is True, 'scaling and reflection'
+    assert similar_triangles([(1, 0), (1, 3), (2, 0)], [(
+        3, 0), (5, 5), (5, 0)]) is False, 'different #2'
+    print("Coding complete? Click 'Check' to earn cool rewards!")
