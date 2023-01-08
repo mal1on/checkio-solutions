fcaffa1c22f735f519904ec9658ed8d616c85027
mal1on <lyubo.malinov@gmail.com>
2023-01-08 16:42:44 +0200

    Mission file created

---
+++ Ice Base/Similar Triangles.py
@@ -0,0 +1,12 @@
+from typing import List, Tuple
+Coords = List[Tuple[int, int]]
+
+
+def similar_triangles(coords_1: Coords, coords_2: Coords) -> bool:
+
+    # your code here
+    return False
+
+
+similar_triangles([(0, 0), (1, 2), (2, 0)], [(3, 0), (4, 2), (5, 0)]) is True
+    
