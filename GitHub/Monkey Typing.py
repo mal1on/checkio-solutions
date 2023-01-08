c6e03541a52a71d1636bc00c8f19ae3cd5ca6545
mal1on <lyubo.malinov@gmail.com>
2023-01-08 11:31:45 +0200

    Checks completed

--- GitHub/Monkey Typing.py
+++ GitHub/Monkey Typing.py
@@ -1,8 +1,19 @@
 def count_words(text: str, words: set) -> int:
-    return 0
+
+    count = []
+
+    for word in words:
+        for item in text.lower().split():
+            if word in item:
+                print(item, word)
+                count.append(word)
+
+    return len(set(count))
 
 
 
 
 count_words("How aresjfhdskfhskd you?", {"how", "are", "you", "hello"}) == 3
-    
+count_words("Bananas, give me bananas!!!", {"banana", "bananas"}) == 2
+count_words("Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+            {"sum", "hamlet", "infinity", "anything"}) == 1
