198cc0746cdf102790eda5175ac66675adfc19f3
mal1on <lyubo.malinov@gmail.com>
2023-01-08 11:34:15 +0200

    Asserts successful and converted to 1-liner

--- GitHub/Monkey Typing.py
+++ GitHub/Monkey Typing.py
@@ -1,19 +1,18 @@
 def count_words(text: str, words: set) -> int:
 
-    count = []
-
-    for word in words:
-        for item in text.lower().split():
-            if word in item:
-                print(item, word)
-                count.append(word)
-
-    return len(set(count))
+    return len(set([word for item in text.lower().split() for word in words if word in item]))
 
 
+if __name__ == '__main__':
+    print("Example:")
+    print(count_words("How aresjfhdskfhskd you?",
+                      {"how", "are", "you", "hello"}))
 
-
-count_words("How aresjfhdskfhskd you?", {"how", "are", "you", "hello"}) == 3
-count_words("Bananas, give me bananas!!!", {"banana", "bananas"}) == 2
-count_words("Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
-            {"sum", "hamlet", "infinity", "anything"}) == 1
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert count_words("How aresjfhdskfhskd you?", {
+                       "how", "are", "you", "hello"}) == 3, "Example"
+    assert count_words("Bananas, give me bananas!!!", {
+                       "banana", "bananas"}) == 2, "BANANAS!"
+    assert count_words("Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+                       {"sum", "hamlet", "infinity", "anything"}) == 1, "Weird text"
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
