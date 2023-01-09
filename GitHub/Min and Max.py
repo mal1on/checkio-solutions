e4ad143030f8f4a071a3e0bcad7b3b83a96aad3d
mal1on <lyubo.malinov@gmail.com>
2023-01-09 21:47:31 +0200

    GitHub - Min and Max mission complete

--- GitHub/Min and Max.py
+++ GitHub/Min and Max.py
@@ -1,5 +1,5 @@
 def min(*args, key=None):
-    
+
     return sorted(args, key=key)[0] if len(args) > 1 else sorted(args[0], key=key)[0]
 
 
@@ -8,9 +8,13 @@ def max(*args, key=None):
     return sorted(args, key=key, reverse=True)[0] if len(args) > 1 else sorted(args[0], key=key, reverse=True)[0]
 
 
-max(3, 2) == 3
-min(3, 2) == 2
-max([1, 2, 0, 3, 4]) == 4
-min("hello") == "e"
-max(2.2, 5.6, 5.9, key=int) == 5.6
-min([[1,2], [3, 4], [9, 0]], key=lambda x: x[1]) == [9, 0]     
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert max(3, 2) == 3, "Simple case max"
+    assert min(3, 2) == 2, "Simple case min"
+    assert max([1, 2, 0, 3, 4]) == 4, "From a list"
+    assert min("hello") == "e", "From string"
+    assert max(2.2, 5.6, 5.9, key=int) == 5.6, "Two maximal items"
+    assert min([[1, 2], [3, 4], [9, 0]], key=lambda x: x[1]) == [
+        9, 0], "lambda key"
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
