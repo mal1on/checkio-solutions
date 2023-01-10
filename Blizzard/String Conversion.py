47e3a0db6341de652a96d97e86a38ba5e81cff58
mal1on <lyubo.malinov@gmail.com>
2023-01-10 12:25:04 +0200

    Blizzard - String Conversion all checks completed

--- Blizzard/String Conversion.py
+++ Blizzard/String Conversion.py
@@ -1,7 +1,22 @@
 def steps_to_convert(line1, line2):
-    return 0
+    
+    changes = 0
+
+    if len(line1) == len(line2):
+        for ind, char in enumerate(line2):
+            if line1[ind] != char:
+                changes += 1
+    else:
+        changes += abs(len(line2) - len(line1))
+
+    print(changes)    
 
 
 
 
-steps_to_convert('line1', 'line2') == 1    
+
+
+steps_to_convert('line1', 'line2') == 1
+steps_to_convert('line1', 'line1') == 0
+steps_to_convert('line1', 'line2') == 1
+steps_to_convert('ine', 'line2') == 2    
