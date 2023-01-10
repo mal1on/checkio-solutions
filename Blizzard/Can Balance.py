fcd1c56457ff3cc9f5de4fa92cb27258bb79f323
mal1on <lyubo.malinov@gmail.com>
2023-01-10 19:13:57 +0200

    Blizzard - Can Balance mission asserts successful

--- Blizzard/Can Balance.py
+++ Blizzard/Can Balance.py
@@ -1,5 +1,6 @@
 from typing import Iterable
 
+
 def can_balance(weights: Iterable) -> int:
     # your code here
     result = -1
@@ -9,20 +10,23 @@ def can_balance(weights: Iterable) -> int:
         weight = 0
         for ind, item in enumerate(comb):
             weight += item * (len(comb) - ind)
-        return weight    
+        return weight
 
     for ind, item in enumerate(weights):
         comb_1, comb_2 = weights[:ind], weights[ind + 1:]
         if w_cal(comb_1) == w_cal(comb_2[::-1]):
             result = ind
 
-    print(result)            
+    return result
 
 
+if __name__ == '__main__':
+    print("Example:")
+    print(can_balance([6, 1, 10, 5, 4]))
 
-
-
-# 6*2 + 1*1 == 5*1 + 4*2
-can_balance([6, 1, 10, 5, 4]) == 2
-# 10*1 == 3*1 + 2*2 + 1*3
-can_balance([10, 3, 3, 2, 1]) == 1
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert can_balance([6, 1, 10, 5, 4]) == 2
+    assert can_balance([10, 3, 3, 2, 1]) == 1
+    assert can_balance([7, 3, 4, 2, 9, 7, 4]) == -1
+    assert can_balance([42]) == 0
+    print("Coding complete? Click 'Check' to earn cool rewards!")
