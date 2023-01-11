6c6f6fb95318a9fd2da2d6b67964ffc581a040d2
mal1on <lyubo.malinov@gmail.com>
2023-01-11 13:57:09 +0200

    GitHub - Ghost Age mission asserts successful

--- GitHub/Ghosts Age.py
+++ GitHub/Ghosts Age.py
@@ -17,17 +17,14 @@ def checkio(opacity):
         else:
             age_opac += 1        
 
-    print(age)                
+    return age        
 
 
-
-
-
-checkio(10000) == 0
-checkio(9999) == 1
-checkio(9997) == 2
-checkio(9994) == 3
-checkio(9995) == 4
-checkio(9990) == 5
-
-    
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio(10000) == 0, "Newborn"
+    assert checkio(9999) == 1, "1 year"
+    assert checkio(9997) == 2, "2 years"
+    assert checkio(9994) == 3, "3 years"
+    assert checkio(9995) == 4, "4 years"
+    assert checkio(9990) == 5, "5 years"    
