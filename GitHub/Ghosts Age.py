08c878e1e6bb1d632a4c912b4cdf3904d775db01
mal1on <lyubo.malinov@gmail.com>
2023-01-11 13:56:23 +0200

    GitHub - Ghost Age mission all checks completed

--- GitHub/Ghosts Age.py
+++ GitHub/Ghosts Age.py
@@ -1,8 +1,33 @@
 def checkio(opacity):
-    return 0
+    age = 0
+    age_opac = 10000
+
+    def fib(limit):
+        fibs = []
+        a, b = 0, 1
+        while a <= limit:
+            fibs.append(a)
+            a, b = b, a + b
+        return fibs    
+
+    while age_opac != opacity:
+        age += 1
+        if age in fib(age):
+            age_opac -= age
+        else:
+            age_opac += 1        
+
+    print(age)                
 
 
 
 
+
+checkio(10000) == 0
+checkio(9999) == 1
+checkio(9997) == 2
+checkio(9994) == 3
+checkio(9995) == 4
 checkio(9990) == 5
+
     
