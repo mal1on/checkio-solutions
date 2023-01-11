ea9c0789d3d4d2bf62de1cb7afde3a47f11c9a3e
mal1on <lyubo.malinov@gmail.com>
2023-01-11 13:59:08 +0200

    GitHub - Ghost Age mission completed

--- GitHub/Ghosts Age.py
+++ GitHub/Ghosts Age.py
@@ -1,4 +1,5 @@
 def checkio(opacity):
+
     age = 0
     age_opac = 10000
 
@@ -8,23 +9,23 @@ def checkio(opacity):
         while a <= limit:
             fibs.append(a)
             a, b = b, a + b
-        return fibs    
+        return fibs
 
     while age_opac != opacity:
         age += 1
         if age in fib(age):
             age_opac -= age
         else:
-            age_opac += 1        
+            age_opac += 1
 
-    return age        
+    return age
 
 
-#These "asserts" using only for self-checking and not necessary for auto-testing
+# These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
     assert checkio(10000) == 0, "Newborn"
     assert checkio(9999) == 1, "1 year"
     assert checkio(9997) == 2, "2 years"
     assert checkio(9994) == 3, "3 years"
     assert checkio(9995) == 4, "4 years"
-    assert checkio(9990) == 5, "5 years"    
+    assert checkio(9990) == 5, "5 years"
