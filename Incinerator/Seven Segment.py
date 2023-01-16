d7fe6299463b1c31b33b3536df986d5d50c16491
mal1on <lyubo.malinov@gmail.com>
2023-01-16 21:11:42 +0200

    Incinerator - Seven Segment - solution updated

--- Incinerator/Seven Segment.py
+++ Incinerator/Seven Segment.py
@@ -5,7 +5,7 @@ ss_dict = {'1' : {'b', 'c'},
             '3' : {'a', 'b', 'g', 'c', 'd'},
             '4' : {'f', 'g', 'b', 'c'},
             '5' : {'a', 'f', 'g', 'c', 'd'},
-            '6' : {'a', 'f', 'f', 'e', 'c', 'd'},
+            '6' : {'a', 'f', 'g', 'e', 'c', 'd'},
             '7' : {'a', 'b', 'c'},
             '8' : {'a', 'b', 'c', 'd', 'e', 'f', 'g'},
             '9' : {'a', 'b', 'c', 'd', 'f', 'g'},
@@ -15,27 +15,29 @@ def seven_segment(lit_seg, broken_seg):
 
     first = {char.lower() for char in lit_seg if char.isupper()}
     second = {char for char in lit_seg if char.islower()}
+    print(first, second)
     broken_u = {char.lower() for char in broken_seg if char.isupper()}
     broken_l = {char for char in broken_seg if char.islower()}
-    first.update(broken_u)
-    second.update(broken_l)
+    first_w_broken = first.copy()
+    second_w_broken = second.copy()
+    first_w_broken.update(broken_u)
+    second_w_broken.update(broken_l)
     first_dig = [] 
     second_dig = [] 
 
     for k, i in ss_dict.items():
-        if i.issubset(first):
+        if i.issubset(first_w_broken) and first.issubset(i):
             first_dig.append(k)
-        if i.issubset(second):
+        if i.issubset(second_w_broken) and second.issubset(i):
             second_dig.append(k)
 
-    first_dig = ['0', '8'] if len(first_dig) == 10 else first_dig
-    second_dig = ['0', '8'] if len(second_dig) == 10 else second_dig
-
-    print(len(list(product(first_dig, second_dig))))     
+    print(len(first_dig) * len(second_dig))     
 
 
 
 
 seven_segment({'B', 'C', 'b', 'c'}, {'A'}) == 2
 seven_segment({'B', 'C', 'a', 'c', 'd', 'f', 'g'}, {'A', 'D', 'G', 'e'}) == 6
-seven_segment({'A', 'B', 'C', 'D', 'E', 'F', 'a', 'b', 'c', 'd', 'e', 'f'}, {'G', 'g'}) == 4  
+seven_segment({'A', 'B', 'C', 'D', 'E', 'F', 'a', 'b', 'c', 'd', 'e', 'f'}, {'G', 'g'}) == 4
+seven_segment({'B', 'C', 'a', 'f', 'g', 'c', 'd'}, {'A', 'G', 'D', 'e'}) == 6 
+seven_segment({'B', 'C', 'a', 'f', 'g', 'c', 'd'}, {'A', 'G', 'D', 'F', 'b', 'e'}) == 20
