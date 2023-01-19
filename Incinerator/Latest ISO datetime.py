81bed6d707740147fe0e7023a6794a161e480ca7
mal1on <lyubo.malinov@gmail.com>
2023-01-19 17:03:11 +0200

    Mine - Latest ISO Datetime mission file created

---
+++ Incinerator/Latest ISO datetime.py
@@ -0,0 +1,7 @@
+def power_supply(network, power_plants):
+    return set([])
+
+
+
+
+power_supply([['p1', 'c1'], ['c1', 'c2']], {'p1': 1}) == set(['с2'])    
---
+++ Mine/Latest ISO datetime.py
@@ -0,0 +1,7 @@
+def get_latest(dt1str: str, dt2str: str) -> str:
+    # your code here
+    return ""
+
+
+
+get_latest("2027-09-01T01:03:10", "1997-04-15T11:18:14") == "2027-09-01T01:03:10"   
