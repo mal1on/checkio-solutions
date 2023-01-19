9f055ee1648f46ed7eb73fd431afb49bff0389fe
mal1on <lyubo.malinov@gmail.com>
2023-01-19 17:20:03 +0200

    Mine - Latest ISO Datetime - mission checks completed

--- Mine/Latest ISO datetime.py
+++ Mine/Latest ISO datetime.py
@@ -1,7 +1,14 @@
+from datetime import datetime
+
 def get_latest(dt1str: str, dt2str: str) -> str:
     # your code here
-    return ""
+    first = datetime.strptime(dt1str, '%Y-%m-%dT%H:%M:%S')
+    second = datetime.strptime(dt2str, '%Y-%m-%dT%H:%M:%S')
+
+    print(first) if first > second else print(second)
 
 
 
-get_latest("2027-09-01T01:03:10", "1997-04-15T11:18:14") == "2027-09-01T01:03:10"   
+get_latest("2027-09-01T01:03:10", "1997-04-15T11:18:14") == "2027-09-01T01:03:10"
+get_latest("2007-03-04T21:08:12", "2007-03-04T21:08:12") == "2007-03-04T21:08:12" 
+get_latest("0001-01-01T01:01:01", "3000-11-16T13:27:02") == "3000-11-16T13:27:02"  
