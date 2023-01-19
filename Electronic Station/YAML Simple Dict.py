e60c830096ea0d40f485efba4153841b88292992
mal1on <lyubo.malinov@gmail.com>
2023-01-19 18:09:55 +0200

    Electronic Station - Yaml. Simple Dict mission file created

---
+++ Electronic Station/YAML Simple Dict.py
@@ -0,0 +1,20 @@
+def yaml(a):
+    
+    result = {}
+
+    for k, v in [b.split(':') for b in a.splitlines() if b]:
+        result[k] = v
+
+    print(result)    
+
+
+
+yaml("""name: Alex
+age: 12""") == {'age': 12, 'name': 'Alex'}
+
+yaml("""name: Alex Fox
+age: 12
+
+class: 12b""") == {'age': 12,
+ 'class': '12b',
+ 'name': 'Alex Fox'}    
