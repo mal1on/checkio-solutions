26c9d6562ed46eaf02b6ed0cffc16de3670ec5ff
mal1on <lyubo.malinov@gmail.com>
2023-01-20 22:36:43 +0200

    Electronic Station - YAML. More Types - mission asserts successful

--- Electronic Station/YAML More Types.py
+++ Electronic Station/YAML More Types.py
@@ -9,23 +9,60 @@ def yaml(a):
         if v.isdigit():
             result[k] = int(v)
         elif v == 'true':
-            result[k] = True 
+            result[k] = True
         elif v == 'false':
             result[k] = False
         elif not v or v == 'null':
             result[k] = None
+        elif '\\' in v:
+            result[k] = v.replace('"', '').replace('\\', '"')
         elif '"' in v:
-            result[k] = v.replace('"', '').replace('\\', '')    
-        else: 
-            result[k] = v       
+            result[k] = v.replace('"', '')
+        else:
+            result[k] = v
+
+    return result
 
 
+if __name__ == "__main__":
+    print("Example:")
+    print(yaml("name: Alex\nage: 12"))
 
-    print(result)
-
-
-yaml('name: "Bob Dylan"\n' "children: 6\n" 'coding: "null" ') == {
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert yaml("name: Alex\nage: 12") == {"age": 12, "name": "Alex"}
+    assert yaml("name: Alex Fox\n" "age: 12\n" "\n" "class: 12b") == {
+        "age": 12,
+        "class": "12b",
+        "name": "Alex Fox",
+    }
+    assert yaml('name: "Alex Fox"\n' "age: 12\n" "\n" "class: 12b") == {
+        "age": 12,
+        "class": "12b",
+        "name": "Alex Fox",
+    }
+    assert yaml('name: "Alex \\"Fox\\""\n' "age: 12\n" "\n" "class: 12b") == {
+        "age": 12,
+        "class": "12b",
+        "name": 'Alex "Fox"',
+    }
+    assert yaml('name: "Bob Dylan"\n' "children: 6\n" "alive: false") == {
+        "alive": False,
+        "children": 6,
+        "name": "Bob Dylan",
+    }
+    assert yaml('name: "Bob Dylan"\n' "children: 6\n" "coding:") == {
+        "children": 6,
+        "coding": None,
+        "name": "Bob Dylan",
+    }
+    assert yaml('name: "Bob Dylan"\n' "children: 6\n" "coding: null") == {
+        "children": 6,
+        "coding": None,
+        "name": "Bob Dylan",
+    }
+    assert yaml('name: "Bob Dylan"\n' "children: 6\n" 'coding: "null" ') == {
         "children": 6,
         "coding": "null",
         "name": "Bob Dylan",
     }
+    print("Coding complete? Click 'Check' to earn cool rewards!")
