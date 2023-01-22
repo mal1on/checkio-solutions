64105dab4db2883d25d506ebee139b4112282275
mal1on <lyubo.malinov@gmail.com>
2023-01-22 22:12:05 +0200

    Electronic Station - YAML. List and Comments - solution updated

--- Electronic Station/YAML List and Comments.py
+++ Electronic Station/YAML List and Comments.py
@@ -1,44 +1,28 @@
 def yaml_converter(yaml_str):
 
-    yaml_str = yaml_str.strip()
+    if yaml_str.strip() in ('', 'null'):
+        return None
+    elif yaml_str[0] in ('"\''):
+        return yaml_str.replace('"', '')
+
+    yaml_str = yaml_str.split('#')[0].strip()
 
     if yaml_str.isdigit():
-        yaml_str = int(yaml_str)
-    elif yaml_str == 'true':
-        yaml_str = True
-    elif yaml_str == 'false':
-        yaml_str = False
-    elif not yaml_str or yaml_str == 'null':
-        yaml_str = None
-    elif '\\' in yaml_str:
-        yaml_str = yaml_str.replace('"', '').replace('\\', '"')
-    elif '"' in yaml_str:
-        yaml_str = yaml_str.replace('"', '')
+        return int(yaml_str)
+    elif yaml_str in ('true', 'false'):
+        return yaml_str == 'true'
 
-    return yaml_str
+    return yaml_str.replace('"', '').replace('\\', '"')
 
 
 def yaml(a):
 
-    lines = [b.split(':')
-             for b in a.splitlines() if b and not b.startswith('#')]
+    lines = [line.strip()
+             for line in a.splitlines() if line and not line.startswith('#')]
 
-    if all(len(line) == 2 for line in lines):
-        result = {}
-        for k, v in lines:
-            result[k] = yaml_converter(v)
-    elif all(line[0].startswith('-') for line in lines):
-        result = []
-        for line in lines:
-            line = line[0].replace('- ', '')
-            if line == '-':
-                result.append(None)
-            elif line.startswith('"') and '#' in line:
-                result.append(line.replace('"', ''))
-            else:
-                result.append(yaml_converter(line.split('#')[0]))
-
-    return result
+    if lines[0].startswith('-'):
+        return [yaml_converter(line[2:]) for line in lines]
+    return {k: yaml_converter(v) for k, v in [l.split(':') for l in lines]}
 
 
 if __name__ == "__main__":
