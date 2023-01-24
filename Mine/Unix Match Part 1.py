ebe9a8f2cd035930f21d103ddf97b511468cb707
mal1on <lyubo.malinov@gmail.com>
2023-01-24 16:05:52 +0200

    Mine - Unix Match. Part 1 - mission completed

--- Mine/Unix Match Part 1.py
+++ Mine/Unix Match Part 1.py
@@ -1,27 +1,32 @@
 import re
 
 re_dict = {'*': '.*', '?': '.'}
-re_special = {'.': '\\.'}
+re_special = ['.']
+
 
 def unix_match(filename: str, pattern: str) -> bool:
-    # your code here
+
     re_pattern = ''
     for char in pattern:
         if char in re_dict.keys():
             re_pattern += re_dict[char]
-        elif char in re_special.keys():
-            re_pattern += re_special[char]
+        elif char in re_special:
+            re_pattern += '\\' + char
         else:
-            re_pattern += char        
+            re_pattern += char
 
-    return True if re.match(re_pattern, filename) else False            
-            
-
+    return True if re.match(re_pattern, filename) else False
 
 
-print(unix_match("somefile.txt", "*")) == True
-print(unix_match("other.exe", "*")) == True 
-print(unix_match("my.exe", "*.txt")) == False
-print(unix_match("log1.txt", "log?.txt")) == True
-print(unix_match('log12.txt', '**')) == True
-print(unix_match('l.txt', '???*')) == True  
+print("Example:")
+print(unix_match("somefile.txt", "*"))
+
+# These "asserts" are used for self-checking
+assert unix_match("somefile.txt", "*") == True
+assert unix_match("other.exe", "*") == True
+assert unix_match("my.exe", "*.txt") == False
+assert unix_match("log1.txt", "log?.txt") == True
+assert unix_match("log12.txt", "log?.txt") == False
+assert unix_match("log12.txt", "log??.txt") == True
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
