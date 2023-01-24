81527d92fac27ab39cb3de47bcb8a1d185754c35
mal1on <lyubo.malinov@gmail.com>
2023-01-24 15:40:17 +0200

    Mine - Unix Match. Part 1 - mission checks completed

--- Mine/Unix Match Part 1.py
+++ Mine/Unix Match Part 1.py
@@ -2,11 +2,20 @@ import re
 
 def unix_match(filename: str, pattern: str) -> bool:
     # your code here
-    return True
+    if pattern == '*' or filename == pattern:
+        return True
+    if pattern.startswith('*'):
+        return filename[- (len(pattern) - 1):] == pattern[1:]
+    if len(filename) == len(pattern) and '?' in pattern:
+        parts = pattern.split('?')
+        return all(part in filename for part in parts)
+
+    return False    
+            
 
 
 
-unix_match("somefile.txt", "*") == True
-unix_match("other.exe", "*") == True 
-unix_match("my.exe", "*.txt") == False
-unix_match("log1.txt", "log?.txt") == True  
+print(unix_match("somefile.txt", "*")) == True
+print(unix_match("other.exe", "*")) == True 
+print(unix_match("my.exe", "*.txt")) == False
+print(unix_match("log1.txt", "log?.txt")) == True  
