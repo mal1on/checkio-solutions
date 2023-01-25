56f6c3458d8edead18da347ea02cd196a6358e34
mal1on <lyubo.malinov@gmail.com>
2023-01-25 21:49:45 +0200

    Mine - Unix Match. Part 2 - mission checks completed

--- Mine/Unix Match Part 2.py
+++ Mine/Unix Match Part 2.py
@@ -1,6 +1,21 @@
+import re
+
+re_dict = {'*': '.*', '?': '.', '!': '^'}
+re_special = ['.']
+
+
 def unix_match(filename: str, pattern: str) -> bool:
-    # your code here
-    return True
+
+    re_pattern = ''
+    for char in pattern:
+        if char in re_dict.keys():
+            re_pattern += re_dict[char]
+        elif char in re_special:
+            re_pattern += '\\' + char
+        else:
+            re_pattern += char
+
+    return True if re.match(re_pattern, filename) else False
 
 
 print(unix_match("log1.txt", "log[1234567890].txt")) == True
