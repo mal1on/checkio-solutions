09ce89e7d27c6262c9dbb8afef87838255a95d99
mal1on <lyubo.malinov@gmail.com>
2023-01-26 13:50:14 +0200

    Mine - Unix Match. Part 3 - asserts successful

--- Mine/Unix Match Part 3.py
+++ Mine/Unix Match Part 3.py
@@ -20,13 +20,19 @@ def unix_match(filename: str, pattern: str) -> bool:
     elif '[^]' in re_pattern:
         re_pattern = '\\[\\!\\]'.join(re_pattern.split('[^]'))    
 
-    print(True if re.match(re_pattern, filename) else False)
+    return True if re.match(re_pattern, filename) else False
 
 
-unix_match('somefile.txt', '*') == True
-unix_match('other.exe', '*') == True
-unix_match('my.exe', '*.txt') == False
-unix_match('log1.txt', 'log?.txt') == True
-unix_match('log1.txt', 'log[1234567890].txt') == True
-unix_match('log12.txt', 'log?.txt') == False
-unix_match('log12.txt', 'log??.txt') == True  
+if __name__ == '__main__':
+    print("Example:")
+    print(unix_match('somefile.txt', '*'))
+    
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert unix_match('somefile.txt', '*') == True
+    assert unix_match('other.exe', '*') == True
+    assert unix_match('my.exe', '*.txt') == False
+    assert unix_match('log1.txt', 'log?.txt') == True
+    assert unix_match('log1.txt', 'log[1234567890].txt') == True
+    assert unix_match('log12.txt', 'log?.txt') == False
+    assert unix_match('log12.txt', 'log??.txt') == True
+    print("Coding complete? Click 'Check' to earn cool rewards!")  
