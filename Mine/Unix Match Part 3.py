91f208ace24ee9a8ebe63963cfde406bef9f508c
mal1on <lyubo.malinov@gmail.com>
2023-01-26 13:49:30 +0200

    Mine - Unix Match. Part 3 - mission checks complete

--- Mine/Unix Match Part 3.py
+++ Mine/Unix Match Part 3.py
@@ -1,7 +1,26 @@
+import re
+
+re_dict = {'*': '.*', '?': '.', '!': '^'}
+re_special = ['.']
+
+
 def unix_match(filename: str, pattern: str) -> bool:
 
-    # your code here
-    return filename == pattern
+    re_pattern = ''
+    for char in pattern:
+        if char in re_dict.keys():
+            re_pattern += re_dict[char]
+        elif char in re_special:
+            re_pattern += '\\' + char
+        else:
+            re_pattern += char
+
+    if '[]' in re_pattern:
+        re_pattern = '\\[\\]'.join(re_pattern.split('[]'))
+    elif '[^]' in re_pattern:
+        re_pattern = '\\[\\!\\]'.join(re_pattern.split('[^]'))    
+
+    print(True if re.match(re_pattern, filename) else False)
 
 
 unix_match('somefile.txt', '*') == True
