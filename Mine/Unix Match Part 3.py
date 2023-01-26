aee3921784792a08741f0dada1ae3313c5138ba0
mal1on <lyubo.malinov@gmail.com>
2023-01-26 20:12:09 +0200

    Mine - Unix Match. Part 3 - mission completed

--- Mine/Unix Match Part 3.py
+++ Mine/Unix Match Part 3.py
@@ -2,28 +2,30 @@ import re
 
 re_dict = {'.': '\\.', '*': '.+', '?': '.', '[!': '[^'}
 
+
 def unix_match(filename: str, pattern: str) -> bool:
 
     re_pattern = pattern
     for key in re_dict:
-        re_pattern = re_pattern.replace(key, re_dict[key]).replace('[.+', '[*').replace('[.', '[?')
+        re_pattern = re_pattern.replace(key, re_dict[key]).replace(
+            '[.+', '[*').replace('[.', '[?')
 
     try:
         return bool(re.match(re_pattern, filename))
     except:
-        return filename == pattern          
-
-    
+        return filename == pattern
 
 
-# unix_match('somefile.txt', '*') == True
-# unix_match('other.exe', '*') == True
-# unix_match('my.exe', '*.txt') == False
-# unix_match('log1.txt', 'log?.txt') == True
-# unix_match('log1.txt', 'log[1234567890].txt') == True
-# unix_match('log12.txt', 'log?.txt') == False
-# unix_match('log12.txt', 'log??.txt') == True
-# print(unix_match("name.txt","name[]txt")) == False
-# print(unix_match("1name.txt","[!1234567890]*")) == False
-# print(unix_match("[!]check.txt","[!]check.txt")) == True
-print(unix_match("[?*]","[[][?][*][]]")) == True
+if __name__ == '__main__':
+    print("Example:")
+    print(unix_match('somefile.txt', '*'))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert unix_match('somefile.txt', '*') == True
+    assert unix_match('other.exe', '*') == True
+    assert unix_match('my.exe', '*.txt') == False
+    assert unix_match('log1.txt', 'log?.txt') == True
+    assert unix_match('log1.txt', 'log[1234567890].txt') == True
+    assert unix_match('log12.txt', 'log?.txt') == False
+    assert unix_match('log12.txt', 'log??.txt') == True
+    print("Coding complete? Click 'Check' to earn cool rewards!")
