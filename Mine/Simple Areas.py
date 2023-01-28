34836e20760d1cb5658fd9bcdcd661e57e2fd878
mal1on <lyubo.malinov@gmail.com>
2023-01-28 12:49:06 +0200

    Mine - Simple Areas - checks complete

--- Mine/Simple Areas.py
+++ Mine/Simple Areas.py
@@ -1,5 +1,16 @@
+from math import pi, sqrt
+
 def simple_areas(*args):
-    return 0
+
+    if len(args) == 1:
+        return pi * (args[0] / 2) ** 2 
+    elif len(args) == 2:
+        return args[0] * args[1]
+    elif len(args) == 3:
+        a, b, c = args[0], args[1], args[2]
+        s = (a + b + c) / 2
+        return sqrt(s * (s - a) * (s - b) * (s - c))
+
 
 
 
