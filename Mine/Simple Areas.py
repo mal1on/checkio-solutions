2d6a1f1f2148b82d72f34ca5b9320f2c518da162
mal1on <lyubo.malinov@gmail.com>
2023-01-28 12:25:57 +0200

    Mine - Simple Areas - mission file created

---
+++ Mine/Simple Areas.py
@@ -0,0 +1,11 @@
+def simple_areas(*args):
+    return 0
+
+
+
+
+simple_areas(3) == 7.07
+simple_areas(2, 2) == 4
+simple_areas(2, 3) == 6
+simple_areas(3, 5, 4) == 6
+simple_areas(1.5, 2.5, 2) == 1.5
