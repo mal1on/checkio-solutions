822f1b2a44bb711ad59c1d0a5fa0e46c321369cf
mal1on <lyubo.malinov@gmail.com>
2023-01-28 17:36:34 +0200

    Mine - Super Root - asserts successful

--- Mine/Super Root.py
+++ Mine/Super Root.py
@@ -3,10 +3,11 @@ def calc(sr, step, number):
     while True:
         sr += step
         if number - 0.001 < sr ** sr < number + 0.001:
-            return sr 
+            return sr
         elif sr ** sr > number:
             sr -= step
             return sr
+
 
 def super_root(number):
 
@@ -15,14 +16,23 @@ def super_root(number):
     while True:
         res = calc(sr, step, number)
         if number - 0.001 < res ** res < number + 0.001:
-            return res   
+            return res
         else:
             sr = res
-            step /= 10    
+            step /= 10
 
 
-print(super_root(4)) == 2
-print(super_root(27)) == 3
-print(super_root(81)) == 3.504339593597054
-print(super_root(2))
-print(super_root(10000000000))
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    def check_result(function, number):
+        result = function(number)
+        if not isinstance(result, (int, float)):
+            print("The result should be a float or an integer.")
+            return False
+        p = result ** result
+        if number - 0.001 < p < number + 0.001:
+            return True
+        return False
+    assert check_result(super_root, 4), "Square"
+    assert check_result(super_root, 9), "Cube"
+    assert check_result(super_root, 81), "Eighty one"
