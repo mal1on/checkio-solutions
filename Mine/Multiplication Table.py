9e072e7c087a1cc7250cb58d0720cc13b0272a5d
mal1on <lyubo.malinov@gmail.com>
2023-01-29 13:39:05 +0200

    Mine - Multiplication Table - checks completed

--- Mine/Multiplication Table.py
+++ Mine/Multiplication Table.py
@@ -1,5 +1,26 @@
 def checkio(first, second):
-    return 1
+    
+    result = {'and': 0, 'or': 0, 'xor': 0}
+    first = list(map(int, str(bin(first)[2:]).lstrip('0')))
+    second = list(map(int, str(bin(second)[2:]).lstrip('0')))
+
+    for f in first:
+
+        and_r, or_r, xor_r = [], [], []
+
+        for s in second:   
+            and_r.append(f and s)
+            or_r.append(f or s)
+            xor_r.append(f ^ s)
+
+        result['and'] += int(''.join(map(str, and_r)), 2)
+        result['or'] += int(''.join(map(str, or_r)), 2)
+        result['xor'] += int(''.join(map(str, xor_r)), 2)
+
+    print(sum(result.values()))        
+        
+
+
 
 
 
