b969f19eae0bc7efac153da5267bdd8b3595a990
mal1on <lyubo.malinov@gmail.com>
2023-01-29 13:39:53 +0200

    Mine - Multiplication Table - asserts successful

--- Mine/Multiplication Table.py
+++ Mine/Multiplication Table.py
@@ -1,5 +1,5 @@
 def checkio(first, second):
-    
+
     result = {'and': 0, 'or': 0, 'xor': 0}
     first = list(map(int, str(bin(first)[2:]).lstrip('0')))
     second = list(map(int, str(bin(second)[2:]).lstrip('0')))
@@ -8,7 +8,7 @@ def checkio(first, second):
 
         and_r, or_r, xor_r = [], [], []
 
-        for s in second:   
+        for s in second:
             and_r.append(f and s)
             or_r.append(f or s)
             xor_r.append(f ^ s)
@@ -17,14 +17,11 @@ def checkio(first, second):
         result['or'] += int(''.join(map(str, or_r)), 2)
         result['xor'] += int(''.join(map(str, xor_r)), 2)
 
-    print(sum(result.values()))        
-        
+    return sum(result.values())
 
 
-
-
-
-
-checkio(4, 6) == 38
-checkio(2, 7) == 28
-checkio(7, 2) == 18    
+# These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio(4, 6) == 38
+    assert checkio(2, 7) == 28
+    assert checkio(7, 2) == 18
