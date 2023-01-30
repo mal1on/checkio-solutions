0bbf486152d581fd4d36601243eb2a290fb2b006
mal1on <lyubo.malinov@gmail.com>
2023-01-30 21:49:09 +0200

    Mine - Count Consecutive Summers - asserts successful

--- Mine/Count Consecutive Summers.py
+++ Mine/Count Consecutive Summers.py
@@ -1,5 +1,5 @@
 def count_consecutive_summers(num):
-    # your code here
+
     diffs = 1
 
     for f_num in range(1, num + 1):
@@ -8,10 +8,16 @@ def count_consecutive_summers(num):
             if f_num == num:
                 diffs += 1
                 break
-    print(diffs)                
+
+    return diffs                
 
 
+if __name__ == '__main__':
+    print("Example:")
+    print(count_consecutive_summers(42))
 
-
-count_consecutive_summers(42) == 4
-count_consecutive_summers(99) == 6    
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert count_consecutive_summers(42) == 4
+    assert count_consecutive_summers(99) == 6
+    assert count_consecutive_summers(1) == 1
+    print("Coding complete? Click 'Check' to earn cool rewards!")   
