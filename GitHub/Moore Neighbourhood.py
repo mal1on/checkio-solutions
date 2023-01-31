c8bae835dc80df1b38400f3aae4604d045343371
mal1on <lyubo.malinov@gmail.com>
2023-01-31 14:18:39 +0200

    Github - Moore Neighbourhood - mission completed

--- GitHub/Moore Neighbourhood.py
+++ GitHub/Moore Neighbourhood.py
@@ -1,27 +1,32 @@
 def count_neighbours(grid, row, col):
-    
+
     count = 0
 
     s = row if row == 0 else row - 1
 
-    for r in grid[s:row+2]:
+    for r in grid[s:row + 2]:
         for i, e in enumerate(r):
-            if e and (i == col or i == col - 1 or i == col +1):
-                  count += 1
+            if e and (i == col or i == col - 1 or i == col + 1):
+                count += 1
 
-    print(count if grid[row][col] == 0 else count - 1)                
-
-
+    return count if grid[row][col] == 0 else count - 1
 
 
-count_neighbours(((1, 0, 0, 1, 0),
-                  (0, 1, 0, 0, 0),
-                  (0, 0, 1, 0, 1),
-                  (1, 0, 0, 0, 0),
-                  (0, 0, 1, 0, 0),), 1, 2) == 3
-
-count_neighbours(((1, 0, 0, 1, 0),
-                  (0, 1, 0, 0, 0),
-                  (0, 0, 1, 0, 1),
-                  (1, 0, 0, 0, 0),
-                  (0, 0, 1, 0, 0),), 0, 0) == 1
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert count_neighbours(((1, 0, 0, 1, 0),
+                             (0, 1, 0, 0, 0),
+                             (0, 0, 1, 0, 1),
+                             (1, 0, 0, 0, 0),
+                             (0, 0, 1, 0, 0),), 1, 2) == 3, "1st example"
+    assert count_neighbours(((1, 0, 0, 1, 0),
+                             (0, 1, 0, 0, 0),
+                             (0, 0, 1, 0, 1),
+                             (1, 0, 0, 0, 0),
+                             (0, 0, 1, 0, 0),), 0, 0) == 1, "2nd example"
+    assert count_neighbours(((1, 1, 1),
+                             (1, 1, 1),
+                             (1, 1, 1),), 0, 2) == 3, "Dense corner"
+    assert count_neighbours(((0, 0, 0),
+                             (0, 1, 0),
+                             (0, 0, 0),), 1, 1) == 0, "Single"
