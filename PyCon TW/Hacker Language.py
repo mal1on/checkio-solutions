892d69398a66d2e373f3b15656b19d0af6c6611a
mal1on <lyubo.malinov@gmail.com>
2023-02-04 21:19:59 +0200

    PyCon TW - Hacker Language - mission completed

--- PyCon TW/Hacker Language.py
+++ PyCon TW/Hacker Language.py
@@ -42,11 +42,15 @@ class HackerLanguage:
         return decrypt(msg)
 
 
-message_1 = HackerLanguage()
-message_1.write('Remember: 21.07.2018 at 11:11AM')
-message_1.delete(2)
-message_1.write('PM')
-message_1.send() == '10100101100101110110111001011101101110001011001011110010:100000021.07.2018100000011000011110100100000011:1110100001001101'
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
 
-message_2 = HackerLanguage()
-message_2.read('10011011111001100000011001011101101110000111010011101100100000011010011110011100000011011011110010.11100101101111110001011011111110100@11001111101101110000111010011101100.110001111011111101101')
+    message_1 = HackerLanguage()
+    message_1.write("secrit")
+    message_1.delete(2)
+    message_1.write("et")
+    message_2 = HackerLanguage()
+
+    assert message_1.send() == "111001111001011100011111001011001011110100"
+    assert message_2.read("11001011101101110000111010011101100") == "email"
+    print("Coding complete? Let's try tests!")
