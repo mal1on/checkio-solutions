34127b243a169b22aaf4bda60aabe27c17b5f74c
mal1on <lyubo.malinov@gmail.com>
2023-02-04 17:17:45 +0200

    PyCon TW - Geometry Figures - mission completed

--- PyCon TW/Geometry Figures.py
+++ PyCon TW/Geometry Figures.py
@@ -1,5 +1,6 @@
 from math import pi
 
+
 class Parameters:
 
     def __init__(self, p):
@@ -9,7 +10,7 @@ class Parameters:
         self.fig = fig
 
     def area(self):
-        
+
         return round(self.fig.area(self.p), 2)
 
     def perimeter(self):
@@ -28,27 +29,27 @@ class Circle:
         return pi * par ** 2
 
     def perimeter(self, par):
-        
+
         return 2 * pi * par
 
     def volume(self, par):
-    
-        return 0        
+
+        return 0
 
 
 class Triangle:
-    
+
     def area(self, par):
 
-        return par ** 2 * 0.433
+        return par ** 2 * 0.4333
 
     def perimeter(self, par):
-        
+
         return par * 3
 
     def volume(self, par):
-    
-        return 0    
+
+        return 0
 
 
 class Square:
@@ -58,40 +59,43 @@ class Square:
         return par * par
 
     def perimeter(self, par):
-        
+
         return par * 4
 
     def volume(self, par):
-    
+
         return 0
+
 
 class Pentagon:
 
     def area(self, par):
 
-        return par ** 2 * 1.72
+        return par ** 2 * 1.7205
 
     def perimeter(self, par):
-        
+
         return par * 5
 
     def volume(self, par):
-    
+
         return 0
+
 
 class Hexagon:
 
     def area(self, par):
 
-        return par ** 2 * 2.598
+        return par ** 2 * 2.5981
 
     def perimeter(self, par):
-        
+
         return par * 6
 
     def volume(self, par):
-    
+
         return 0
+
 
 class Cube:
 
@@ -100,32 +104,35 @@ class Cube:
         return 6 * par ** 2
 
     def perimeter(self, par):
-        
+
         return 12 * par
 
     def volume(self, par):
-    
+
         return par ** 3
 
 
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
 
+    figure = Parameters(10)
 
-figure = Parameters(10)
-    
-figure.choose_figure(Circle())
-print(figure.area()) == 314.16
+    figure.choose_figure(Circle())
+    assert figure.area() == 314.16
 
-figure.choose_figure(Triangle())
-print(figure.perimeter()) == 30
+    figure.choose_figure(Triangle())
+    assert figure.perimeter() == 30
 
-figure.choose_figure(Square())
-print(figure.area()) == 100
+    figure.choose_figure(Square())
+    assert figure.area() == 100
 
-figure.choose_figure(Pentagon())
-print(figure.perimeter()) == 50
+    figure.choose_figure(Pentagon())
+    assert figure.perimeter() == 50
 
-figure.choose_figure(Hexagon())
-print(figure.perimeter()) == 60
+    figure.choose_figure(Hexagon())
+    assert figure.perimeter() == 60
 
-figure.choose_figure(Cube())
-print(figure.volume()) == 1000
+    figure.choose_figure(Cube())
+    assert figure.volume() == 1000
+
+    print("Coding complete? Let's try tests!")
