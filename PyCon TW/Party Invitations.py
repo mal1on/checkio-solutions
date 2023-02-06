be43f9586292d5d1f1c9ae6b9641273464469505
mal1on <lyubo.malinov@gmail.com>
2023-02-06 17:03:17 +0200

    PyCon TW - Party Invitations mission file created

---
+++ PyCon TW/Party Invitations.py
@@ -0,0 +1,26 @@
+class Friend:
+    pass
+
+class Party:
+    pass
+
+
+
+# party = Party("Midnight Pub")
+# nick = Friend("Nick")
+# john = Friend("John")
+# lucy = Friend("Lucy")
+# chuck = Friend("Chuck")
+
+# party.add_friend(nick)
+# party.add_friend(john)
+# party.add_friend(lucy)
+# party.send_invites("Friday, 9:00 PM")
+# party.del_friend(nick)
+# party.send_invites("Saturday, 10:00 AM")
+# party.add_friend(chuck)
+
+# john.show_invite() == "Midnight Pub: Saturday, 10:00 AM"
+# lucy.show_invite() == "Midnight Pub: Saturday, 10:00 AM"
+# nick.show_invite() == "Midnight Pub: Friday, 9:00 PM"
+# chuck.show_invite() == "No party..."    
