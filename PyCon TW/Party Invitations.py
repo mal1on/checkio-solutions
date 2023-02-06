8f23839237615905fc50a6112e28be3d5f69b55b
mal1on <lyubo.malinov@gmail.com>
2023-02-06 17:36:15 +0200

    PyCon TW - Party Invitations mission file created

--- PyCon TW/Party Invitations.py
+++ PyCon TW/Party Invitations.py
@@ -1,26 +1,40 @@
 class Friend:
-    pass
+    def __init__(self, name): 
+        self.invite = 'No party...'
+    def get_invite(self, invite):
+        self.invite = invite
+    def show_invite(self):
+        return self.invite        
+
 
 class Party:
-    pass
+    def __init__(self, place):
+        self.place = place
+        self.friends = []
+    def add_friend(self, friend):
+        self.friends.append(friend)
+    def del_friend(self, friend):
+        self.friends.remove(friend)
+    def send_invites(self, invite):
+        for friend in self.friends:
+            friend.get_invite(f'{self.place}: {invite}')                
 
 
+party = Party("Midnight Pub")
+nick = Friend("Nick")
+john = Friend("John")
+lucy = Friend("Lucy")
+chuck = Friend("Chuck")
 
-# party = Party("Midnight Pub")
-# nick = Friend("Nick")
-# john = Friend("John")
-# lucy = Friend("Lucy")
-# chuck = Friend("Chuck")
+party.add_friend(nick)
+party.add_friend(john)
+party.add_friend(lucy)
+party.send_invites("Friday, 9:00 PM")
+party.del_friend(nick)
+party.send_invites("Saturday, 10:00 AM")
+party.add_friend(chuck)
 
-# party.add_friend(nick)
-# party.add_friend(john)
-# party.add_friend(lucy)
-# party.send_invites("Friday, 9:00 PM")
-# party.del_friend(nick)
-# party.send_invites("Saturday, 10:00 AM")
-# party.add_friend(chuck)
-
-# john.show_invite() == "Midnight Pub: Saturday, 10:00 AM"
-# lucy.show_invite() == "Midnight Pub: Saturday, 10:00 AM"
-# nick.show_invite() == "Midnight Pub: Friday, 9:00 PM"
-# chuck.show_invite() == "No party..."    
+john.show_invite() == "Midnight Pub: Saturday, 10:00 AM"
+lucy.show_invite() == "Midnight Pub: Saturday, 10:00 AM"
+nick.show_invite() == "Midnight Pub: Friday, 9:00 PM"
+chuck.show_invite() == "No party..."    
