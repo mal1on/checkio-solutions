3786b21977b6b5f0811e511f237228502964d140
mal1on <lyubo.malinov@gmail.com>
2023-02-07 20:42:38 +0200

    PyCon TW - Text Editor - mission checks completed

--- PyCon TW/Text Editor.py
+++ PyCon TW/Text Editor.py
@@ -1,20 +1,35 @@
 class Text:
-    pass
+    def __init__(self):
+        self.text = ''
+        self.font = ''
+    def write(self, text):
+        self.text += text
+    def set_font(self, font):
+        self.font = f'[{font}]'
+    def show(self):
+        return f'{self.font}{self.text}{self.font}'
+    def restore(self, number):
+        self.text = number[0]
+        self.font = number[1]                 
 
 class SavedText:
-    pass
-
+    def __init__(self):
+        self.revisions = []
+    def save_text(self, text):
+        self.revisions.append((text.text, text.font))
+    def get_version(self, number):
+        return self.revisions[number]        
 
 
 text = Text()
 saver = SavedText()
-    
-# text.write("At the very beginning ")
-# saver.save_text(text)
-# text.set_font("Arial")
-# saver.save_text(text)
-# text.write("there was nothing.")
-# text.show() == "[Arial]At the very beginning there was nothing.[Arial]"
-    
-# text.restore(saver.get_version(0))
-# text.show() == "At the very beginning "    
+
+text.write("At the very beginning ")
+saver.save_text(text)
+text.set_font("Arial")
+saver.save_text(text)
+text.write("there was nothing.")
+print(text.show()) == "[Arial]At the very beginning there was nothing.[Arial]"
+
+text.restore(saver.get_version(0))
+print(text.show()) == "At the very beginning "
