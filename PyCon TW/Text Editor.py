cfad4c161facfd9aa49abc916942e4ab0bafe666
mal1on <lyubo.malinov@gmail.com>
2023-02-07 19:59:47 +0200

    PyCon TW - Text Editor - mission file created

---
+++ PyCon TW/Text Editor.py
@@ -0,0 +1,20 @@
+class Text:
+    pass
+
+class SavedText:
+    pass
+
+
+
+text = Text()
+saver = SavedText()
+    
+# text.write("At the very beginning ")
+# saver.save_text(text)
+# text.set_font("Arial")
+# saver.save_text(text)
+# text.write("there was nothing.")
+# text.show() == "[Arial]At the very beginning there was nothing.[Arial]"
+    
+# text.restore(saver.get_version(0))
+# text.show() == "At the very beginning "    
