6c5fd24e645e12c2d9a6b45b2b2b498919667054
mal1on <lyubo.malinov@gmail.com>
2023-02-08 20:54:40 +0200

    The Warriors - solution updated

---
+++ PyCon TW/The Warriors.py
@@ -0,0 +1,43 @@
+class Warrior:
+    def __init__(self):
+        self.health = 50
+        self.attack = 5
+
+    @property
+    def is_alive(self):
+        return self.health > 0
+
+
+class Knight(Warrior):
+    def __init__(self):
+        self.health = 50
+        self.attack = 7
+
+
+def fight(w1, w2):
+    while w1.is_alive and w2.is_alive:
+        w2.health -= w1.attack
+        if w2.is_alive:
+            w1.health -= w2.attack
+    return w1.is_alive
+
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+
+    chuck = Warrior()
+    bruce = Warrior()
+    carl = Knight()
+    dave = Warrior()
+    mark = Warrior()
+
+    assert fight(chuck, bruce) == True
+    assert fight(dave, carl) == False
+    assert chuck.is_alive == True
+    assert bruce.is_alive == False
+    assert carl.is_alive == True
+    assert dave.is_alive == False
+    assert fight(carl, mark) == False
+    assert carl.is_alive == False
+
+    print("Coding complete? Let's try tests!")
