1e1a9090e2e8dded1fdcf593001ffd0374593c17
mal1on <lyubo.malinov@gmail.com>
2023-02-11 15:13:02 +0200

    Letter Queue - mission completed

--- PyCon TW/Letter Queue.py
+++ PyCon TW/Letter Queue.py
@@ -13,14 +13,27 @@ def letter_queue(commands):
     return ''.join(result)
 
 
-letter_queue(['PUSH A',
-              'POP',
-              'POP',
-              'PUSH Z',
-              'PUSH D',
-              'PUSH O',
-              'POP',
-              'PUSH T']) == 'DOT'
-letter_queue(['POP', 'POP']) == ''
-letter_queue(['PUSH H', 'PUSH I']) == 'HI'
-letter_queue([]) == ''
+if __name__ == '__main__':
+    print("Example:")
+    print(letter_queue(['PUSH A',
+                        'POP',
+                        'POP',
+                        'PUSH Z',
+                        'PUSH D',
+                        'PUSH O',
+                        'POP',
+                        'PUSH T']))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert letter_queue(['PUSH A',
+                         'POP',
+                         'POP',
+                         'PUSH Z',
+                         'PUSH D',
+                         'PUSH O',
+                         'POP',
+                         'PUSH T']) == 'DOT'
+    assert letter_queue(['POP', 'POP']) == ''
+    assert letter_queue(['PUSH H', 'PUSH I']) == 'HI'
+    assert letter_queue([]) == ''
+    print("Coding complete? Click 'Check' to earn cool rewards!")
