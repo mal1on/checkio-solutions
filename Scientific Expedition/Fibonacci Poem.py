ec6de3214236155e8b5091abd277296cc39bad3b
mal1on <lyubo.malinov@gmail.com>
2023-02-16 18:37:49 +0200

    Fibonacci Poem - mission checks completed

--- Scientific Expedition/Fibonacci Poem.py
+++ Scientific Expedition/Fibonacci Poem.py
@@ -1,6 +1,20 @@
 def fibo_poem(text: str) -> str:
+    result = ''
+    text = text.split()
+    a, b = 0, 1
+    while a < len(text):
+        a, b = b, a + b
+        result += (''.join(text[:a]) + '\n')
+        text = text[a:]
+
+    if text:
+        text += '_' * (b - len(text))
+        result += (''.join(text) + '\n')
+
+    print(result.rstrip())
 
 
-
-
-fibo_poem("There are three kinds of lies: Lies, damned lies, and the benchmarks.")
+fibo_poem("") == ""
+fibo_poem("Django framework") == "Django\nframework"
+fibo_poem("Zen of Python") == "Zen\nof\nPython _"
+fibo_poem("There are three kinds of lies: Lies, damned lies, and the benchmarks.") == "There\nare\nthree kinds\nof lies: Lies,\ndamned lies, and the benchmarks."
