6462169ee0462e10a5e079903d4eaeb35a5b67da
mal1on <lyubo.malinov@gmail.com>
2023-02-16 20:56:47 +0200

    Fibonacci Poem - mission completed

--- Scientific Expedition/Fibonacci Poem.py
+++ Scientific Expedition/Fibonacci Poem.py
@@ -2,19 +2,27 @@ def fibo_poem(text: str) -> str:
     result = ''
     text = text.split()
     a, b = 0, 1
-    while a < len(text):
+    while b < len(text):
         a, b = b, a + b
-        result += (''.join(text[:a]) + '\n')
+        result += (' '.join(text[:a]) + '\n')
         text = text[a:]
 
-    if text:
-        text += '_' * (b - len(text))
-        result += (''.join(text) + '\n')
+    if text: result += ' '.join(text) + ' _' * (b - len(text))
 
-    print(result.rstrip())
+    return result.rstrip()
 
 
-fibo_poem("") == ""
-fibo_poem("Django framework") == "Django\nframework"
-fibo_poem("Zen of Python") == "Zen\nof\nPython _"
-fibo_poem("There are three kinds of lies: Lies, damned lies, and the benchmarks.") == "There\nare\nthree kinds\nof lies: Lies,\ndamned lies, and the benchmarks."
+print("Example:")
+print(fibo_poem("Zen of Python"))
+
+# These "asserts" are used for self-checking
+assert fibo_poem("") == ""
+assert fibo_poem("Django framework") == "Django\nframework"
+assert fibo_poem("Zen of Python") == "Zen\nof\nPython _"
+assert (
+    fibo_poem(
+        "There are three kinds of lies: Lies, damned lies, and the benchmarks.")
+    == "There\nare\nthree kinds\nof lies: Lies,\ndamned lies, and the benchmarks."
+)
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
