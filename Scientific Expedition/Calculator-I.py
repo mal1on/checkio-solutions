fcfbc92e98f1529498f4d599759f054cd743e207
mal1on <lyubo.malinov@gmail.com>
2023-02-16 21:15:08 +0200

    Calculator-I - mission file created

---
+++ Scientific Expedition/Calculator-I.py
@@ -0,0 +1,10 @@
+def calculator(log: str) -> str:
+    # your code here
+    return ""
+
+
+
+calculator("000000") == "0"
+calculator("0000123") == "123"
+calculator("12") == "12"
+calculator("+12") == "12"
