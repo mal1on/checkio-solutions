619b9092aa3d1fbf7c7fd75f4144e65087520a27
mal1on <lyubo.malinov@gmail.com>
2023-02-17 12:49:48 +0200

    Calculator-I - mission checks completed

--- Scientific Expedition/Calculator-I.py
+++ Scientific Expedition/Calculator-I.py
@@ -1,10 +1,24 @@
-def calculator(log: str) -> str:
-    # your code here
-    return ""
+def calculator(log):
+    screen = ''
+    ops = ['=', '+', '-']
+    log = log.lstrip('0')
+    log = log if log else '0'
+    for ch in log:
+        if ch.isnumeric():
+            screen += ch
+
+    print(screen)
 
 
 
-calculator("000000") == "0"
-calculator("0000123") == "123"
-calculator("12") == "12"
-calculator("+12") == "12"
+# calculator("000000") == "0"
+# calculator("0000123") == "123"
+# calculator("12") == "12"
+# calculator("+12") == "12"
+# calculator("") == "0"
+# calculator("1+2") == "2"
+# calculator("2+") == "2"
+# calculator("1+2=") == "3"
+# calculator("1+2-") == "3"
+# calculator("1+2=2") == "2"
+calculator("=5=10=15") == "15"
