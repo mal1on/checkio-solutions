eb243cc0cd1838a75a228193a5d803377d26ece0
mal1on <lyubo.malinov@gmail.com>
2023-02-18 11:50:05 +0200

    Calculator-II - mission file created

---
+++ Scientific Expedition/Calculator-II.py
@@ -0,0 +1,49 @@
+from operator import add, sub
+from re import findall
+
+
+def my_reduce(operators, numbers):
+    nums = iter(numbers)
+    ops = iter(operators)
+    value = next(nums)
+    for element in nums:
+        op = next(ops)
+        match op:
+            case '+':
+                function = add
+            case '-':
+                function = sub
+            case '=':
+                def function(a, b): return b
+        value = function(value, element)
+    return value
+
+
+def calculator(log):
+
+    log = log if log else '0'
+    nums = list(map(int, findall('\\d+', log)))
+    nums = nums if log[0].isdigit() else [0] + nums
+    ops = findall('\\D', log)
+
+    print(str(nums[-1]) if log[-1].isdigit() else str(my_reduce(ops, nums)))
+
+
+# calculator("000000") == "0"
+# calculator("0000123") == "123"
+# calculator("12") == "12"
+# calculator("+12") == "12"
+# calculator("") == "0"
+# calculator("1+2") == "2"
+# calculator("2+") == "2"
+# calculator("1+2=") == "3"
+# calculator("1+2-") == "3"
+# calculator("1+2=2") == "2"
+# calculator("=5=10=15") == "15"
+# calculator('000005+003=') == '8'
+# calculator('-5-10+15') == '15'
+
+calculator("3+=") == "6"
+calculator("3+2==") == "7"
+calculator("3+-2=") == "1"
+calculator("-=-+3-++--+-2=-") == "1"
