da1f29aaf156b88c85fcff6be81353aa961d73e4
mal1on <lyubo.malinov@gmail.com>
2023-02-20 21:09:56 +0200

    Reveal the Number - mission file created

---
+++ Scientific Expedition/Reveal the Number.py
@@ -0,0 +1,10 @@
+def reveal_num(line):
+
+
+
+
+
+reveal_num("F0(t}") == 0
+reveal_num("Utc&g") == None
+reveal_num("-aB%|_-+2ADS.12+3.ADS1.2") == 2.12312
+reveal_num("-aB%|_+-2ADS.12+3.ADS1.2") == -2.12312
