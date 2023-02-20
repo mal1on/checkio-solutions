fb4f2d667678db0ff7d16a7728a9f6fed3dfc18c
mal1on <lyubo.malinov@gmail.com>
2023-02-20 17:53:07 +0200

    Calculator-III - mission completed

--- Scientific Expedition/Calculator-III.py
+++ Scientific Expedition/Calculator-III.py
@@ -28,38 +28,44 @@ def my_reduce(operators, numbers):
         opss = plusminus(opss)
         prev_op = None
         for oop in opss:
-            #print(value, oop, element, prev_op)
             if oop == '+':
                 function = add
             elif oop == '-':
                 function = sub
             elif oop == '==':
                 value = function(value, numbers[ind])
-                if abs(value) > 99999: return 'error'
+                if abs(value) > 99999:
+                    return 'error'
                 continue
             elif oop == '=':
                 if prev_op:
                     value = function(value, numbers[ind])
-                    if abs(value) > 99999: return 'error'
+                    if abs(value) > 99999:
+                        return 'error'
                     continue
-                else:
+                elif len(opss) > 1:
                     def function(a, b): return a
+                else:
+                    def function(a, b): return b
             elif oop == '+=':
                 function = add
                 value = function(value, value)
-                if value > 99999: return 'error'
+                if value > 99999:
+                    return 'error'
                 prev_op = oop
                 continue
             elif oop == '-=':
                 function = sub
                 value = function(value, value)
-                if abs(value) > 99999: return 'error'
+                if abs(value) > 99999:
+                    return 'error'
                 prev_op = oop
                 continue
             else:
                 def function(a, b): return b
             value = function(value, element)
-            if value > 99999: return 'error'
+            if value > 99999:
+                return 'error'
             prev_op = oop
     return value
 
@@ -67,37 +73,41 @@ def my_reduce(operators, numbers):
 def calculator(log):
 
     log = log if log else '0'
-    log = findall('-\\d+.*|\\d+.*',  log)[0]
+    log = log if log and any(i.isdigit() for i in log) else '0'
     nums = [int(str(num)[:5]) for num in list(map(int, findall('\\d+', log)))]
     nums = [0] + nums if log[0] == '-' else nums
     ops = findall('\\D+', log)
-    nums = nums if (ops and not any(ops[-1].startswith(i) for i in ['==', '+=', '-='])) or all(e.isdigit() for e in log) == 1 else nums + [0]
+    nums = nums if (ops and not any(ops[-1].startswith(i) for i in [
+                    '==', '+=', '-='])) or all(e.isdigit() for e in log) == 1 else nums + [0]
 
     return str(nums[-1]) if log[-1].isdigit() else str(my_reduce(ops, nums))
 
 
-print(calculator("90000+10000=")) == "error"
-print(calculator("90000+10000-10000=")) == "error"
-print(calculator("90000+10000-10000")) == "10000"
-print(calculator("123456789")) == "12345"
-print(calculator("123456789+5=")) == "12350"
-print(calculator("5+123456789")) == "12345"
-print(calculator("50000+=")) == "error"
-print(calculator("3+=")) == "6"
-print(calculator("3+2==")) == "7"
-print(calculator("3+-2=")) == "1"
-print(calculator("-=-+3-++--+-2=-")) == "1"
-print(calculator("000000")) == "0"
-print(calculator("0000123")) == "123"
-print(calculator("12")) == "12"
-print(calculator("+12")) == "12"
-print(calculator("")) == "0"
-print(calculator("1+2")) == "2"
-print(calculator("2+")) == "2"
-print(calculator("1+2=")) == "3"
-print(calculator("1+2-")) == "3"
-print(calculator("1+2=2")) == "2"
-print(calculator("=5=10=15")) == "15"
-print(calculator('50000-====')) == 'error'
-print(calculator('+-=12=')) == '12'
-print(calculator('2+3=+7=')) == '12'
+print("Example:")
+print(calculator("100000"))
+
+# These "asserts" are used for self-checking
+assert calculator("90000+10000=") == "error"
+assert calculator("90000+10000-10000=") == "error"
+assert calculator("90000+10000-10000") == "10000"
+assert calculator("123456789") == "12345"
+assert calculator("123456789+5=") == "12350"
+assert calculator("5+123456789") == "12345"
+assert calculator("50000+=") == "error"
+assert calculator("3+=") == "6"
+assert calculator("3+2==") == "7"
+assert calculator("3+-2=") == "1"
+assert calculator("-=-+3-++--+-2=-") == "1"
+assert calculator("000000") == "0"
+assert calculator("0000123") == "123"
+assert calculator("12") == "12"
+assert calculator("+12") == "12"
+assert calculator("") == "0"
+assert calculator("1+2") == "2"
+assert calculator("2+") == "2"
+assert calculator("1+2=") == "3"
+assert calculator("1+2-") == "3"
+assert calculator("1+2=2") == "2"
+assert calculator("=5=10=15") == "15"
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
