11285372bbf376a992035da1bf8e5dca778792cc
mal1on <lyubo.malinov@gmail.com>
2023-02-21 11:45:45 +0200

    The Hamming Distance - mission completed

--- GitHub/The Hamming Distance.py
+++ GitHub/The Hamming Distance.py
@@ -1,11 +1,11 @@
 def checkio(n, m):
     n = str(bin(n))[2:].zfill(20)
     m = str(bin(m))[2:].zfill(20)
-    print(len(list(filter(lambda a: len(set(a)) > 1, zip(n, m)))))
+    return len(list(filter(lambda a: len(set(a)) > 1, zip(n, m))))
 
 
-
-
-checkio(117, 17) == 3
-checkio(1, 2) == 2
-checkio(16, 15) == 5
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio(117, 17) == 3, "First example"
+    assert checkio(1, 2) == 2, "Second example"
+    assert checkio(16, 15) == 5, "Third example"
