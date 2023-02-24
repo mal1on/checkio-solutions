687004afebc695d4a823b82ecd0b8e7416f20134
mal1on <lyubo.malinov@gmail.com>
2023-02-24 22:37:49 +0200

    Morse Clock - mission completed

--- Scientific Expedition/Morse Clock.py
+++ Scientific Expedition/Morse Clock.py
@@ -7,11 +7,18 @@ def checkio(time_string: str) -> str:
     s = s.zfill(2)
     s = bin(int(s[0]))[2:].zfill(3) + ' ' + bin(int(s[1]))[2:].zfill(4)
 
-    print((h + ' : ' + m + ' : ' + s).replace('1', '-').replace('0', '.'))
+    return (h + ' : ' + m + ' : ' + s).replace('1', '-').replace('0', '.')
 
 
+if __name__ == '__main__':
+    print("Example:")
+    print(checkio("10:37:49"))
 
-checkio("10:37:49") == ".- .... : .-- .--- : -.. -..-"
-checkio("21:34:56") == "-. ...- : .-- .-.. : -.- .--."
-checkio("00:1:02") == ".. .... : ... ...- : ... ..-."
-checkio("23:59:59") == "-. ..-- : -.- -..- : -.- -..-"
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio("10:37:49") == ".- .... : .-- .--- : -.. -..-", "First Test"
+    assert checkio(
+        "21:34:56") == "-. ...- : .-- .-.. : -.- .--.", "Second Test"
+    assert checkio("00:1:02") == ".. .... : ... ...- : ... ..-.", "Third Test"
+    assert checkio(
+        "23:59:59") == "-. ..-- : -.- -..- : -.- -..-", "Fourth Test"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
