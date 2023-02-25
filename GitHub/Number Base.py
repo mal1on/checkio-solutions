bcd95d7927eefc615f574d9a84c99ffd194cc766
mal1on <lyubo.malinov@gmail.com>
2023-02-25 15:24:19 +0200

    Number Base - mission file created

---
+++ GitHub/Number Base.py
@@ -0,0 +1,11 @@
+def checkio(str_number: str, radix: int) -> int:
+    return -1
+
+
+
+
+checkio("AF", 16) == 175
+checkio("101", 2) == 5
+checkio("101", 5) == 26
+checkio("Z", 36) == 35
+checkio("AB", 10) == -1
