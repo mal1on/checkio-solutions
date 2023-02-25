c3f64be370ddacc0c1fc6d91b0da06c798137420
mal1on <lyubo.malinov@gmail.com>
2023-02-25 23:44:16 +0200

    IP Network: Route Summarization - all checks completed

--- Dropbox/IP Network Route Summarization.py
+++ Dropbox/IP Network Route Summarization.py
@@ -4,19 +4,27 @@ def binary_prefix(prefix):
 
 def checkio(data):
     b_prefixes = [binary_prefix(p) for p in data]
-    mask = ''
+    b_mask = []
     for i in zip(*b_prefixes):
         if len(set(i)) == 1:
-            mask += i[0]
+            b_mask.append(i[0])
         else:
             break
-    print(mask, len(mask))
 
+    subnet = str(len(b_mask))
+    d_mask, octet = '', ''
+    while b_mask:
+        if len(octet) < 8:
+            octet += b_mask.pop(0)
+        elif len(octet) == 8:
+            d_mask += str(int(octet, 2)) + '.'
+            octet = ''
+    d_mask += str(int(octet[::-1].zfill(8)[::-1], 2))
+    mask = d_mask + (4 - len(d_mask.split('.'))) * '.0'
 
-
-
+    print(mask + '/' + subnet)
 
 
 checkio(["172.16.12.0", "172.16.13.0", "172.16.14.0", "172.16.15.0"]) == "172.16.12.0/22"
-# checkio(["172.16.12.0", "172.16.13.0", "172.155.43.9"]) == "172.0.0.0/8"
-# checkio(["172.16.12.0", "172.16.13.0", "172.155.43.9", "146.11.2.2"]) == "128.0.0.0/2"
+checkio(["172.16.12.0", "172.16.13.0", "172.155.43.9"]) == "172.0.0.0/8"
+checkio(["172.16.12.0", "172.16.13.0", "172.155.43.9", "146.11.2.2"]) == "128.0.0.0/2"
