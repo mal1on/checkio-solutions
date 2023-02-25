10fcfb5cb46eab8eead6968139c61aa16573f542
mal1on <lyubo.malinov@gmail.com>
2023-02-25 15:37:35 +0200

    Number Base - mission completed

--- GitHub/Number Base.py
+++ GitHub/Number Base.py
@@ -1,14 +1,18 @@
 def checkio(str_number, radix):
     try:
         return int(str_number, radix)
-    except:
+    except ValueError:
         return -1
 
 
+if __name__ == '__main__':
+    print('Example:')
+    print(checkio("AF", 16))
 
-
-checkio("AF", 16) == 175
-checkio("101", 2) == 5
-checkio("101", 5) == 26
-checkio("Z", 36) == 35
-checkio("AB", 10) == -1
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio("AF", 16) == 175, "Hex"
+    assert checkio("101", 2) == 5, "Bin"
+    assert checkio("101", 5) == 26, "5 base"
+    assert checkio("Z", 36) == 35, "Z base"
+    assert checkio("AB", 10) == -1, "B > A = 10"
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
