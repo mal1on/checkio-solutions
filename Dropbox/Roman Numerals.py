3a3e063263ce7318138093e7c2250507c9bf84f2
mal1on <lyubo.malinov@gmail.com>
2023-03-03 13:23:35 +0200

    Roman Numerals - mission checks completed

--- Dropbox/Roman Numerals.py
+++ Dropbox/Roman Numerals.py
@@ -1,5 +1,20 @@
-def checkio(data):
+rn = {'1':'I', '5':'V', '10':'X', '50':'L', '100':'C', '500':'D', '1000':'M'}
 
+def checkio(data):
+    m = ["", "M", "MM", "MMM"]
+    c = ["", "C", "CC", "CCC", "CD", "D",
+         "DC", "DCC", "DCCC", "CM"]
+    x = ["", "X", "XX", "XXX", "XL", "L",
+         "LX", "LXX", "LXXX", "XC"]
+    i = ["", "I", "II", "III", "IV", "V",
+         "VI", "VII", "VIII", "IX"]
+
+    ths = m[data // 1000]
+    hs = c[(data % 1000) // 100]
+    ts = x[(data % 100) // 10]
+    os = i[data % 10]
+
+    print(ths+hs+ts+os)
 
 
 
