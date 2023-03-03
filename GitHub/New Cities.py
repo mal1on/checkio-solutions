544473a04bf09058a99a1ad7de69529828147ff3
mal1on <lyubo.malinov@gmail.com>
2023-03-03 15:53:04 +0200

    New Cities - mission checks completed

--- GitHub/New Cities.py
+++ GitHub/New Cities.py
@@ -1,30 +1,25 @@
 def subnetworks(net, crushes):
-    subs = []
     for crush in crushes:
-        for a, b in net:
-            if crush == a:
-                subs.append([b])
-            elif crush == b:
-                subs.append([a])
-            elif [a] not in subs and [b] not in subs:
-                subs.append([a, b])
-    print(subs)
+        for link in net:
+            if crush in link:
+                link.remove(crush)
+    return len(set(node for link in net for node in link)) - 1
 
 
 
 
 
-# subnetworks([
-#         ['A', 'B'],
-#         ['B', 'C'],
-#         ['C', 'D']
-#     ], ['B']) == 2
-# subnetworks([
-#         ['A', 'B'],
-#         ['A', 'C'],
-#         ['A', 'D'],
-#         ['D', 'F']
-#     ], ['A']) == 3
+subnetworks([
+        ['A', 'B'],
+        ['B', 'C'],
+        ['C', 'D']
+    ], ['B']) == 2
+subnetworks([
+        ['A', 'B'],
+        ['A', 'C'],
+        ['A', 'D'],
+        ['D', 'F']
+    ], ['A']) == 3
 subnetworks([
         ['A', 'B'],
         ['B', 'C'],
