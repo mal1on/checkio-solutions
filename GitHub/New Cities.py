9c3982c577c6dc59a6ed8c012068b29cc4338ea2
mal1on <lyubo.malinov@gmail.com>
2023-03-03 16:20:46 +0200

    New Cities - extra mission check completed

--- GitHub/New Cities.py
+++ GitHub/New Cities.py
@@ -3,25 +3,18 @@ def subnetworks(net, crushes):
         for link in net:
             if crush in link:
                 link.remove(crush)
-    return len(set(node for link in net for node in link)) - 1
+    singles = [sub for sub in net if len(sub) == 1]
+    doubles = [sub for sub in net if len(sub) == 2]
+    print(singles, doubles)
+    subs = len(doubles)
+    for single in singles:
+        if single[0] not in [node for link in doubles for node in link]:
+            subs += 1
+    print(subs)
 
 
 
-
-
-subnetworks([
-        ['A', 'B'],
-        ['B', 'C'],
-        ['C', 'D']
-    ], ['B']) == 2
-subnetworks([
-        ['A', 'B'],
-        ['A', 'C'],
-        ['A', 'D'],
-        ['D', 'F']
-    ], ['A']) == 3
-subnetworks([
-        ['A', 'B'],
-        ['B', 'C'],
-        ['C', 'D']
-    ], ['C', 'D']) == 1
+subnetworks([['A', 'B'],['B', 'C'],['C', 'D']], ['B']) == 2
+subnetworks([['A', 'B'],['A', 'C'],['A', 'D'],['D', 'F']], ['A']) == 3
+subnetworks([['A', 'B'],['B', 'C'],['C', 'D']], ['C', 'D']) == 1
+subnetworks([["A","B"],["A","C"],["A","D"],["D","F"],["B","C"]],["A"]) == 2
