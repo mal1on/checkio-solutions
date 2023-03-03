cc8c873a62b7db22d44cd1e7356039ceed0590f1
mal1on <lyubo.malinov@gmail.com>
2023-03-03 18:26:34 +0200

    The Flat Dictionary - mission checks completed

--- Electronic Station/The Flat Dictionary.py
+++ Electronic Station/The Flat Dictionary.py
@@ -1,9 +1,14 @@
-def flatten(dictionary):
-    # your code here
-    return {}
+def flatten(d, key=''):
+    items = [] if d else [(key, '')]
+    for k, v in d.items():
+        new_key = key + '/' + k if key else k
+        try:
+            items.extend(flatten(v, new_key).items())
+        except:
+            items.append((new_key, v))
+    return dict(items)
 
 
-
-flatten({"key": "value"}) == {"key": "value"}
-flatten({"key": {"deeper": {"more": {"enough": "value"}}}}) == {"key/deeper/more/enough": "value"}
-flatten({"empty": {}}) == {"empty": ""}
+# print(flatten({"key": "value"})) == {"key": "value"}
+# print(flatten({"key": {"deeper": {"more": {"enough": "value"}}}})) == {"key/deeper/more/enough": "value"}
+print(flatten({"empty": {}})) == {"empty": ""}
