2ba8ae99ec6cb6745585f62420cab0d3a7350eca
mal1on <lyubo.malinov@gmail.com>
2023-03-19 21:35:27 +0200

    Making Change - added inf check

--- Mine/Making Change.py
+++ Mine/Making Change.py
@@ -2,18 +2,23 @@ def checkio(price, denominations):
     """
         return the minimum number of coins that add up to the price
     """
-    result = []
+    result = float('inf')
 
     for i in range(len(denominations)):
         temp = []
         for coin in denominations[::-1][i:]:
-            while sum(temp + [coin]) <= price:
+            while sum(temp + [coin]) <= price and len(temp) < result:
                 temp.append(coin)
-        result.append(temp)
+        if sum(temp) == price:
+            result = len(temp)
+        print(temp)
 
-    print(len(min(result, key=len)))
+    print(result if result < float('inf') else None)
 
 
 
 checkio(8, [1, 3, 5]) == 2
 checkio(12, [1, 4, 5]) == 3
+checkio(1,[3,4,5]) == None
+checkio(4,[3,5]) == None
+checkio(123456,[1,6,7,456,678]) == 187
