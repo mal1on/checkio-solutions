06b6cfcd8752dac07b66dc97661390795887115c
mal1on <lyubo.malinov@gmail.com>
2023-03-21 20:27:22 +0200

    Say Hi - mission file created

---
+++ Polygon/Say Hi.py
@@ -0,0 +1,7 @@
+def say_hi(name, age):
+
+    return "Hi. My name is Alex and I'm 32 years old"
+
+
+say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old"
+say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old"
