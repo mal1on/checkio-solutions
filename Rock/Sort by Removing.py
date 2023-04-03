047149ec4557d41ea75ef9637642e675d3f30c88
mal1on <lyubo.malinov@gmail.com>
2023-04-03 20:11:43 +0300

    Sort by Removing - mission completed

--- Rock/Sort by Removing.py
+++ Rock/Sort by Removing.py
@@ -8,13 +8,19 @@ def sort_by_removing(values: list) -> list:
         if i >= next_val:
             result.append(i)
             next_val = i
-    print(result)
+    return result
 
 
+if __name__ == '__main__':
+    print("Example:")
+    print(sort_by_removing([3, 5, 2, 6]))
 
-
-sort_by_removing([3, 5, 2, 6]) == [3, 5, 6]
-sort_by_removing([7, 6, 5, 4, 3, 2, 1]) == [7]
-sort_by_removing([3, 3, 3, 3]) == [3, 3, 3, 3]
-sort_by_removing([5, 6, 7, 0, 7, 0, 10]) == [5, 6, 7, 7, 10]
-sort_by_removing([1, 5, 2, 3, 4, 7, 8]) == [1, 5, 7, 8]
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert sort_by_removing([3, 5, 2, 6]) == [3, 5, 6]
+    assert sort_by_removing([7, 6, 5, 4, 3, 2, 1]) == [7]
+    assert sort_by_removing([3, 3, 3, 3]) == [3, 3, 3, 3]
+    assert sort_by_removing([5, 6, 7, 0, 7, 0, 10]) == [5, 6, 7, 7, 10]
+    assert sort_by_removing([1, 5, 2, 3, 4, 7, 8]) == [1, 5, 7, 8]
+    assert sort_by_removing([1, 7, 2, 3, 4, 5]) == [1, 7]
+    assert sort_by_removing([]) == []
+    print("Coding complete? Click 'Check' to earn cool rewards!")
