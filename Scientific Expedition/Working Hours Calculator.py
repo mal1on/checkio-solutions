da35cb71e67b8a49298240e60f3ef18feeaf0256
mal1on <lyubo.malinov@gmail.com>
2023-04-07 16:38:31 +0300

    Working Hours Calculator - mission completed

--- Scientific Expedition/Working Hours Calculator.py
+++ Scientific Expedition/Working Hours Calculator.py
@@ -15,12 +15,20 @@ def working_hours(date1, date2, start_time, end_time, holy):
     days = (dt.strptime(date2, '%Y-%m-%d') -
             dt.strptime(date1, '%Y-%m-%d')).days + 1 - holy
 
-    print(round(days * (daily.seconds / 3600), 2))
+    return round(days * (daily.seconds / 3600), 2)
 
 
-working_hours("2023-03-01", "2023-03-01", "09:00", "17:00", []) == 8
-working_hours("2023-03-01", "2023-03-02", "09:00", "17:00", []) == 16
-working_hours("2023-03-01", "2023-03-03", "09:00",
-              "17:00", ["2023-03-01"]) == 16
-working_hours("2023-03-01", "2023-03-05", "08:45",
-              "17:10", ["2023-03-03"]) == 16.83
+print("Example:")
+print(working_hours("2023-03-01", "2023-03-01", "09:00", "17:00", []))
+
+# These "asserts" are used for self-checking
+assert working_hours("2023-03-01", "2023-03-01", "09:00", "17:00", []) == 8
+assert working_hours("2023-03-01", "2023-03-02", "09:00", "17:00", []) == 16
+assert working_hours("2023-03-01", "2023-03-03", "09:00",
+                     "17:00", ["2023-03-01"]) == 16
+assert (
+    working_hours("2023-03-01", "2023-03-05", "08:45",
+                  "17:10", ["2023-03-03"]) == 16.83
+)
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
