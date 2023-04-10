89ad3d21219542efcd858dfacc67508802a373ae
mal1on <lyubo.malinov@gmail.com>
2023-04-10 11:22:51 +0300

    Sum Consecutives - mission checks completed

--- Rock/Sum Consecutives.py
+++ Rock/Sum Consecutives.py
@@ -1,8 +1,22 @@
 def sum_consecutives(a):
-    # your code here
-    return None
-
+    if not a:
+        return []
+    else:
+        result = []
+        current = current_sum = a[0]
+        for i in a[1:]:
+            if i == current:
+                current_sum += i
+            else:
+                result.append(current_sum)
+                current_sum = current = i
+        result.append(current_sum)
+        print(result)
 
 
 sum_consecutives([1, 1, 1, 1]) == [4]
 sum_consecutives([1, 1, 2, 2]) == [2, 4]
+sum_consecutives([1, 1, 2, 1]) == [2, 2, 1]
+sum_consecutives([3, 3, 3, 4, 4, 5, 6, 6]) == [9, 8, 5, 12]
+sum_consecutives([1]) == [1]
+sum_consecutives([]) == []
