9b6da4739830feb0ce734cc942f55a875a07a604
mal1on <lyubo.malinov@gmail.com>
2023-04-11 15:37:33 +0300

    Find Quotes - mission checks completed

--- Rock/Find Quotes.py
+++ Rock/Find Quotes.py
@@ -1,8 +1,31 @@
+from re import findall
+
 def find_quotes(a):
-    # your code here
-    return None
+
+    return findall(r'"([^"]*)"', a)
 
 
 
-find_quotes('"Greetings"') == ['Greetings']
-find_quotes('Hi') == []
+print(find_quotes('"Greetings"')) == ['Greetings']
+print(find_quotes('Hi')) == []
+print(find_quotes('"Lorem Ipsum" is simply dummy text '
+ 'of the printing and typesetting '
+ 'industry. Lorem Ipsum has been the '
+ '"industry\'s standard dummy text '
+ 'ever since the 1500s", when an '
+ 'unknown printer took a galley of '
+ 'type and scrambled it to make a type '
+ 'specimen book. It has survived not '
+ 'only five centuries, but also the '
+ 'leap into electronic typesetting, '
+ 'remaining essentially unchanged. "It '
+ 'was popularised in the 1960s" with '
+ 'the release of Letraset sheets '
+ 'containing Lorem Ipsum passages, and '
+ 'more recently with desktop '
+ 'publishing software like Aldus '
+ 'PageMaker including versions of '
+ 'Lorem Ipsum.')) == ['Lorem Ipsum',
+ "industry's standard dummy text ever "
+ 'since the 1500s',
+ 'It was popularised in the 1960s']
