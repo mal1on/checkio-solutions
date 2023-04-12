190f8e2b98b0b60005acc39e603122ce14b050ce
mal1on <lyubo.malinov@gmail.com>
2023-04-12 09:47:14 +0300

    Keywords Finder - mission file created

---
+++ Rock/Keywords Finder.py
@@ -0,0 +1,7 @@
+def checkio(text: str, words: str) -> str:
+    # your code here
+    return ""
+
+
+checkio("This is only a text example for task example.",
+        "example") == "This is only a text <span>example</span> for task <span>example</span>."
