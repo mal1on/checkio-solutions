81af21fc0c4653a626827d1ba37d89b4f8a4c774
mal1on <lyubo.malinov@gmail.com>
2023-04-15 12:26:34 +0300

    Keywords Finder - all checks completed with a new approach

--- Rock/Keywords Finder.py
+++ Rock/Keywords Finder.py
@@ -1,26 +1,50 @@
+import re
+
+
 def checkio(text: str, words: str) -> str:
 
-    temp = ''
-    for word in sorted(words.split(), key=len, reverse=True):
-        for text_word in text.split():
-            if word.lower() in text_word.lower() and 'span' not in text_word:
-                st = text_word.lower().find(word.lower())
-                end = st + len(word)
-                temp += text_word[:st]+'<span>'+text_word[st:end] +'</span>'+text_word[end:] + ' '
-            else:
-                temp += text_word + ' '
-        text = temp
-        temp = ''
-    print(text.strip())
+    found = []
+    check = []
+    result = ''
+
+    for keyword in words.lower().split():
+        matches = re.finditer(keyword, text.lower())
+        for match in matches:
+            found.append(match.span())
+
+    found.sort()
+
+    for ind, find in enumerate(found):
+        f_s, f_e = find
+        for other in found[ind+1:]:
+            o_s, o_e = other
+            if f_s <= o_s <= f_e and o_e > f_e:
+                f_e = o_e
+                found.remove(other)
+            elif f_e >= o_e >= f_s and o_s < f_s:
+                f_s = o_s
+                found.remove(other)
+            elif o_s >= f_s and o_e <= f_e:
+                found.remove(other)
+        check.append((f_s, f_e))
+
+    mult = 0
+    for s, e in check:
+        s += mult * 13
+        e += mult * 13
+        text = text[:s] + '<span>' + text[s:e] + '</span>' + text[e:]
+        mult += 1
+    return text
 
 
-checkio("This is only a text example for task example.",
-        "example") == "This is only a text <span>example</span> for task <span>example</span>."
-checkio("Python is a widely used high-level programming language.",
-        "pyThoN") == "<span>Python</span> is a widely used high-level programming language."
-checkio("It is experiment for control groups with similar distributions.",
-        "is im") == "It <span>is</span> exper<span>im</span>ent for control groups with s<span>im</span>ilar d<span>is</span>tributions."
-checkio("The National Aeronautics and Space Administration (NASA).",
-        "nasa  THE") == "<span>The</span> National Aeronautics and Space Administration (<span>NASA</span>)."
-checkio("Did you find anything?", "word space tree") == "Did you find anything?"
-checkio("Hello World! Or LOL", "hell world or lo") == "<span>Hello</span> <span>World</span>! <span>Or</span> <span>LO</span>L"
+print(checkio("This is only a text example for task example.",
+        "example") == "This is only a text <span>example</span> for task <span>example</span>.")
+print(checkio("Python is a widely used high-level programming language.",
+        "pyThoN") == "<span>Python</span> is a widely used high-level programming language.")
+print(checkio("It is experiment for control groups with similar distributions.",
+        "is im") == "It <span>is</span> exper<span>im</span>ent for control groups with s<span>im</span>ilar d<span>is</span>tributions.")
+print(checkio("The National Aeronautics and Space Administration (NASA).",
+        "nasa  THE") == "<span>The</span> National Aeronautics and Space Administration (<span>NASA</span>).")
+print(checkio("Did you find anything?", "word space tree") == "Did you find anything?")
+print(checkio("Hello World! Or LOL",
+        "hell world or lo") == "<span>Hello</span> <span>World</span>! <span>Or</span> <span>LO</span>L")
