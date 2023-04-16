c052d387b18e17b589d9e06f32eb31968f4327a4
mal1on <lyubo.malinov@gmail.com>
2023-04-16 20:25:28 +0300

    Halloween Monsters - mission file created

---
+++ Rock/Halloween Monsters.py
@@ -0,0 +1,20 @@
+MONSTERS = """
+skeleton
+ghost
+jack
+vampire
+witch
+mummy
+zombie
+werewolf
+frankenstein
+"""
+
+
+def halloween_monsters(spell: str) -> int:
+
+    return 0
+
+
+halloween_monsters('tkjagchso') == 2               # jack, ghost
+halloween_monsters('finhtiistchwwaerecnnkt') == 3  # witch, witch, frankenstein
