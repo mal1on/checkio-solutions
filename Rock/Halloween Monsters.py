e7757859860aaa85134ffdf31056d919c52cea9a
mal1on <lyubo.malinov@gmail.com>
2023-04-16 21:14:51 +0300

    Halloween Monsters - all checks completed

--- Rock/Halloween Monsters.py
+++ Rock/Halloween Monsters.py
@@ -1,19 +1,20 @@
-MONSTERS = """
-skeleton
-ghost
-jack
-vampire
-witch
-mummy
-zombie
-werewolf
-frankenstein
-"""
+import re
 
 
 def halloween_monsters(spell: str) -> int:
 
-    return 0
+    monsters = sorted(['skeleton', 'ghost', 'jack', 'vampire',
+                'witch', 'mummy', 'zombie', 'werewolf', 'frankenstein'], key=len)
+
+    found = []
+    for monster in monsters:
+        while all(c in spell for c in monster):
+            found.append(monster)
+            for c in set(monster):
+                spell = spell.replace(c, '', monster.count(c))
+    print(found, spell)
+
+
 
 
 halloween_monsters('tkjagchso') == 2               # jack, ghost
