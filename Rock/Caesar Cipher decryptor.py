dc57841f3d6a541e8d5e0db2e6a90f253ce24064
mal1on <lyubo.malinov@gmail.com>
2023-04-30 15:40:33 +0300

    Caesar Cipher decryptor - mission file created

---
+++ Rock/Caesar Cipher decryptor.py
@@ -0,0 +1,11 @@
+def to_decrypt(cryptotext, delta):
+    #replace this for solution
+    return cryptotext
+
+
+
+to_decrypt("!d! [e] &f*", -3) == "a b c"
+to_decrypt("x^$# y&*( (z):-)", 3) == "a b c"
+to_decrypt("iycfbu!@# junj%&", -16) == "simple text"
+to_decrypt("*$#%swzybdkxd !)(^#%dohd", -10) == "important text"
+to_decrypt("fgngr **&&frperg^__^", 13) == "state secret"
