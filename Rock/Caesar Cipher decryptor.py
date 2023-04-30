69b0a176c41c3a95d8d04552b91def880c677d8c
mal1on <lyubo.malinov@gmail.com>
2023-04-30 15:50:53 +0300

    Caesar Cipher decryptor - all checks successful

--- Rock/Caesar Cipher decryptor.py
+++ Rock/Caesar Cipher decryptor.py
@@ -1,7 +1,19 @@
-def to_decrypt(cryptotext, delta):
-    #replace this for solution
-    return cryptotext
+from string import ascii_lowercase as alphabet
 
+def to_decrypt(cryptotext, delta):
+
+    result = ''
+    for char in cryptotext:
+        if char in alphabet:
+            index = alphabet.find(char) + delta
+            if index >= 26:
+                index = delta - (26 - alphabet.find(char))
+            result += alphabet[index]
+        elif char == ' ':
+            result += char
+
+
+    print(result)
 
 
 to_decrypt("!d! [e] &f*", -3) == "a b c"
