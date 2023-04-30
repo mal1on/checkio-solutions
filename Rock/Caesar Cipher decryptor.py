df9ff3b5a799d1fb741ed0f520439a466dcf706d
mal1on <lyubo.malinov@gmail.com>
2023-04-30 15:52:41 +0300

    Caesar Cipher decryptor - mission completed

--- Rock/Caesar Cipher decryptor.py
+++ Rock/Caesar Cipher decryptor.py
@@ -1,5 +1,6 @@
 from string import ascii_lowercase as alphabet
 
+
 def to_decrypt(cryptotext, delta):
 
     result = ''
@@ -12,12 +13,17 @@ def to_decrypt(cryptotext, delta):
         elif char == ' ':
             result += char
 
+    return result
 
-    print(result)
 
+if __name__ == '__main__':
+    print("Example:")
+    print(to_decrypt('abc', 10))
 
-to_decrypt("!d! [e] &f*", -3) == "a b c"
-to_decrypt("x^$# y&*( (z):-)", 3) == "a b c"
-to_decrypt("iycfbu!@# junj%&", -16) == "simple text"
-to_decrypt("*$#%swzybdkxd !)(^#%dohd", -10) == "important text"
-to_decrypt("fgngr **&&frperg^__^", 13) == "state secret"
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert to_decrypt("!d! [e] &f*", -3) == "a b c"
+    assert to_decrypt("x^$# y&*( (z):-)", 3) == "a b c"
+    assert to_decrypt("iycfbu!@# junj%&", -16) == "simple text"
+    assert to_decrypt("*$#%swzybdkxd !)(^#%dohd", -10) == "important text"
+    assert to_decrypt("fgngr **&&frperg^__^", 13) == "state secret"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
