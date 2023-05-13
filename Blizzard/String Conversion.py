cde1037ce51746181fdf8f46c3de6b7c9acea574
mal1on <lyubo.malinov@gmail.com>
2023-05-13 18:24:59 +0300

    String Conversion - solution updated

--- Blizzard/String Conversion.py
+++ Blizzard/String Conversion.py
@@ -1,13 +1,11 @@
 def steps_to_convert(line1, line2):
 
-    changes = 0
+    changes = abs(len(line1) - len(line2))
 
-    if len(line1) == len(line2):
-        for ind, char in enumerate(line2):
-            if line1[ind] != char:
+    if line1 not in line2 and line2 not in line1:
+        for couple in zip(line1, line2):
+            if len(set(couple)) > 1:
                 changes += 1
-    else:
-        changes += abs(len(line2) - len(line1))
 
     return changes
 
