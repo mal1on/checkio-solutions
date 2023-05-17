856279590ae0f8a7c3cae5a622ce2cbc64fc5efb
mal1on <lyubo.malinov@gmail.com>
2023-05-17 21:19:25 +0300

    Digits Doublets - all checks completed

--- Alice in Wonderland/Digits Doublets.py
+++ Alice in Wonderland/Digits Doublets.py
@@ -1,5 +1,33 @@
+from collections import deque
+
+
+def diffs(num1, num2):
+
+    return sum([1 for ind, dig in enumerate(str(num1)) if str(num2)[ind] != dig])
+
+
+def min_diffs(num1, elements):
+
+    count = [(el, diffs(el, num1)) for el in elements]
+    return min(count, key=lambda a: a[1])[0]
+
+
 def checkio(numbers):
-    return []
+
+    chain = deque([numbers[-1]])
+    elements = {number:[n for n in numbers[:-1] if diffs(number, n) == 1] for number in numbers}
+
+    while chain[0] != numbers[0]:
+        for number in numbers:
+            if number == chain[0]:
+                close_nums = [num for num in elements[number] if num not in chain]
+                count = [(el, diffs(el, numbers[0])) for el in close_nums]
+                chain.appendleft(min(count, key=lambda a: a[1])[0])
+    print(list(chain))
+
+
+
+
 
 
 
