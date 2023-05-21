87a419df9e99f2de3bd2e0d25324e0259d5e814d
mal1on <lyubo.malinov@gmail.com>
2023-05-21 16:46:12 +0300

    Weekend Counter - mission file created

---
+++ Master of The Time Stone/Weekend Counter.py
@@ -0,0 +1,17 @@
+from datetime import date
+
+
+def checkio(from_date, to_date):
+    """
+        Count the days of rest
+    """
+    return 0
+
+
+
+
+
+
+checkio(date(2013, 9, 18), date(2013, 9, 23)) == 2
+checkio(date(2013, 1, 1), date(2013, 2, 1)) == 8
+checkio(date(2013, 2, 2), date(2013, 2, 3)) == 2
