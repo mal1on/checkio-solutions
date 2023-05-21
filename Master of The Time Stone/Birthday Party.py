5ded264b064074ec366bf011f9d785b7cbf68a44
mal1on <lyubo.malinov@gmail.com>
2023-05-21 13:55:43 +0300

    Birthday Party - mission completed

--- Master of The Time Stone/Birthday Party.py
+++ Master of The Time Stone/Birthday Party.py
@@ -8,8 +8,13 @@ def birthday_party(birthday: datetime.date) -> datetime.date:
     return birthday + datetime.timedelta(6 - birthday.isoweekday())
 
 
-print(birthday_party(datetime.date(2022, 1, 5))) == datetime.date(2022, 1, 8)
-print(birthday_party(datetime.date(2022, 2, 21))) == datetime.date(2022, 2, 26)
-print(birthday_party(datetime.date(2022, 3, 26))) == datetime.date(2022, 3, 26)
-print(birthday_party(datetime.date(2022, 4, 17))) == datetime.date(2022, 4, 17)
-print(birthday_party(datetime.date(2022, 5, 31)))
+print('Example:')
+print(birthday_party(datetime.date(2022, 1, 5)))
+
+assert birthday_party(datetime.date(2022, 1, 5)) == datetime.date(2022, 1, 8)
+assert birthday_party(datetime.date(2022, 2, 21)) == datetime.date(2022, 2, 26)
+assert birthday_party(datetime.date(2022, 3, 26)) == datetime.date(2022, 3, 26)
+assert birthday_party(datetime.date(2022, 4, 17)) == datetime.date(2022, 4, 17)
+assert birthday_party(datetime.date(2022, 3, 30)) == datetime.date(2022, 4, 2)
+
+print("The first mission is done! Click 'Check' to earn cool rewards!")
