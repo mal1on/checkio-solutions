f6444ba3aa7931a235e43bec1d9eb8d39df5a685
mal1on <lyubo.malinov@gmail.com>
2023-05-21 18:51:30 +0300

    The First Working Day - all checks successful

--- Master of The Time Stone/The First Working Day.py
+++ Master of The Time Stone/The First Working Day.py
@@ -1,9 +1,17 @@
-from datetime import date, timedelta
+from datetime import datetime, timedelta
 
 
 def vacation(date, days):
     #replace this for solution
-    return 0
+    fwd = datetime.strptime(date, '%Y-%m-%d') + timedelta(days)
+    match fwd.isoweekday():
+        case 6:
+            fwd += timedelta(2)
+        case 7:
+            fwd += timedelta(1)
+
+
+    print(fwd.date())
 
 
 
