983b85aa532b572df5efd8e6fc394e8a9feff1f5
mal1on <lyubo.malinov@gmail.com>
2023-05-21 18:04:42 +0300

    The First Working Day - mission file created

---
+++ Master of The Time Stone/The First Working Day.py
@@ -0,0 +1,13 @@
+from datetime import date, timedelta
+
+
+def vacation(date, days):
+    #replace this for solution
+    return 0
+
+
+
+vacation('2018-07-01', 14) == '2018-07-16'
+vacation('2018-02-19', 10) == '2018-03-01'
+vacation('2000-02-28', 5) == '2000-03-06'
+vacation('1999-12-20', 14) == '2000-01-03'
