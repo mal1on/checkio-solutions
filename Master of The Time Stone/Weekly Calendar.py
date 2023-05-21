62d0184d6f8e0f578b0726eb75854520c61a24e6
mal1on <lyubo.malinov@gmail.com>
2023-05-22 00:22:51 +0300

    Weekly Calendar - all checks successful

--- Master of The Time Stone/Weekly Calendar.py
+++ Master of The Time Stone/Weekly Calendar.py
@@ -1,9 +1,19 @@
+from datetime import date, timedelta as td
+
+
 def weekly_calendar(year, month, day, firstweekday):
 
-    return []
+    current = date(year, month, day)
+    week = []
+    fwd = (current - td(current.weekday() - firstweekday))
+    for d in range(7):
+        week.append((fwd + td(d)).day)
+    print(week)
+    return week
 
 
 
 list(weekly_calendar(2020, 1, 1, 0)) == [30, 31, 1, 2, 3, 4, 5]
 list(weekly_calendar(2020, 9, 20, 6)) == [20, 21, 22, 23, 24, 25, 26]
 list(weekly_calendar(2020, 9, 30, 0)) == [28, 29, 30, 1, 2, 3, 4]
+list(weekly_calendar(2020, 2, 29, 2)) == [26, 27, 28, 29, 1, 2, 3]
