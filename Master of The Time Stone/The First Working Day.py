4c645f26bdfa4ba8469926516081b091eaf33bd5
mal1on <lyubo.malinov@gmail.com>
2023-05-21 19:02:47 +0300

    The First Working Day - mission completed

--- Master of The Time Stone/The First Working Day.py
+++ Master of The Time Stone/The First Working Day.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 
 
 def vacation(date, days):
-    #replace this for solution
+
     fwd = datetime.strptime(date, '%Y-%m-%d') + timedelta(days)
     match fwd.isoweekday():
         case 6:
@@ -10,12 +10,16 @@ def vacation(date, days):
         case 7:
             fwd += timedelta(1)
 
-
-    print(fwd.date())
-
+    return fwd.date().strftime('%Y-%m-%d')
 
 
-vacation('2018-07-01', 14) == '2018-07-16'
-vacation('2018-02-19', 10) == '2018-03-01'
-vacation('2000-02-28', 5) == '2000-03-06'
-vacation('1999-12-20', 14) == '2000-01-03'
+if __name__ == '__main__':
+    print("Example:")
+    print(vacation('2018-07-01', 14))
+
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert vacation('2018-07-01', 14) == '2018-07-16'
+    assert vacation('2018-02-19', 10) == '2018-03-01'
+    assert vacation('2000-02-28', 5) == '2000-03-06'
+    assert vacation('1999-12-20', 14) == '2000-01-03'
+    print("Coding complete? Click 'Check' to earn cool rewards!")
