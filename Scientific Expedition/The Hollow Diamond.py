3bfdc2bf8f554c16cd545787d8355b80fab062b8
mal1on <lyubo.malinov@gmail.com>
2023-05-24 23:30:50 +0300

    The Hollow Diamond - all checks successful

--- Scientific Expedition/The Hollow Diamond.py
+++ Scientific Expedition/The Hollow Diamond.py
@@ -11,17 +11,18 @@ def hollow_diamond(side: int, length: int, cw: bool) -> str:
         inner.append(inner[-1] + 2)
     inner +=  inner[:-1][::-1]
     for i in range(len(outer)):
-        diamond += outer[i] * ' ' + letters.popleft() + inner[i] * ' ' + letters.pop() + '\n'
+        if cw:
+            diamond += outer[i] * ' ' + letters.pop() + inner[i] * ' ' + letters.popleft() + '\n'
+        else:
+            diamond += outer[i] * ' ' + letters.popleft() + inner[i] * ' ' + letters.pop() + '\n'
     diamond += (side - 1) * ' ' + letters.pop()
-
-    # diamond += (side - 1) * ' ' + letters[2 * side - 2]
 
     print(diamond)
 
 
 
 
-# hollow_diamond(3, 8, True) == "  a\n h b\ng   c\n f d\n  e"
+hollow_diamond(3, 8, True) == "  a\n h b\ng   c\n f d\n  e"
 hollow_diamond(3, 6, False) == "  a\n b *\nc   *\n d f\n  e"
 hollow_diamond(4, 10, False) == "   a\n  b *\n c   *\nd     j\n e   i\n  f h\n   g"
-# hollow_diamond(5, 16, True) == "    a\n   p b\n  o   c\n n     d\nm       e\n l     f\n  k   g\n   j h\n    i"
+hollow_diamond(5, 16, True) == "    a\n   p b\n  o   c\n n     d\nm       e\n l     f\n  k   g\n   j h\n    i"
