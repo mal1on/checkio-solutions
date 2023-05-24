ef4fc3f049a27001ca5575f3dd82ba21cce65031
mal1on <lyubo.malinov@gmail.com>
2023-05-24 23:28:05 +0300

    The Hollow Diamond - couter clockwise diamond created

--- Scientific Expedition/The Hollow Diamond.py
+++ Scientific Expedition/The Hollow Diamond.py
@@ -1,10 +1,27 @@
+from string import ascii_lowercase as alphabet
+from collections import deque
+
 def hollow_diamond(side: int, length: int, cw: bool) -> str:
-    # your code here
-    return ""
+
+    letters = deque(alphabet[:length] + (4 * side - 4 - length) * '*')
+    diamond = (side - 1) * ' ' + letters.popleft() + '\n'
+    outer = list(range(side - 2, -1, -1)) + list(range(1, side - 1))
+    inner = [1]
+    for el in range(side - 2):
+        inner.append(inner[-1] + 2)
+    inner +=  inner[:-1][::-1]
+    for i in range(len(outer)):
+        diamond += outer[i] * ' ' + letters.popleft() + inner[i] * ' ' + letters.pop() + '\n'
+    diamond += (side - 1) * ' ' + letters.pop()
+
+    # diamond += (side - 1) * ' ' + letters[2 * side - 2]
+
+    print(diamond)
 
 
 
-hollow_diamond(3, 8, True) == "  a\n h b\ng   c\n f d\n  e"
+
+# hollow_diamond(3, 8, True) == "  a\n h b\ng   c\n f d\n  e"
 hollow_diamond(3, 6, False) == "  a\n b *\nc   *\n d f\n  e"
-(hollow_diamond(4, 10, False) == "   a\n  b *\n c   *\nd     j\n e   i\n  f h\n   g")
-(hollow_diamond(5, 16, True) == "    a\n   p b\n  o   c\n n     d\nm       e\n l     f\n  k   g\n   j h\n    i")
+hollow_diamond(4, 10, False) == "   a\n  b *\n c   *\nd     j\n e   i\n  f h\n   g"
+# hollow_diamond(5, 16, True) == "    a\n   p b\n  o   c\n n     d\nm       e\n l     f\n  k   g\n   j h\n    i"
