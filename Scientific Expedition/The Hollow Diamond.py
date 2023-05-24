90668ad010f33d387fbf463e7dc954e78d71e7e1
mal1on <lyubo.malinov@gmail.com>
2023-05-24 20:18:26 +0300

    The Hollow Diamond - mission file created

---
+++ Scientific Expedition/The Hollow Diamond.py
@@ -0,0 +1,10 @@
+def hollow_diamond(side: int, length: int, cw: bool) -> str:
+    # your code here
+    return ""
+
+
+
+hollow_diamond(3, 8, True) == "  a\n h b\ng   c\n f d\n  e"
+hollow_diamond(3, 6, False) == "  a\n b *\nc   *\n d f\n  e"
+(hollow_diamond(4, 10, False) == "   a\n  b *\n c   *\nd     j\n e   i\n  f h\n   g")
+(hollow_diamond(5, 16, True) == "    a\n   p b\n  o   c\n n     d\nm       e\n l     f\n  k   g\n   j h\n    i")
