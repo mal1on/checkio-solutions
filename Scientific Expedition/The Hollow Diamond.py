9c5d93cc7b973b4723556f72217220db60609460
mal1on <lyubo.malinov@gmail.com>
2023-05-24 23:33:49 +0300

    The Hollow Diamond - mission completed

--- Scientific Expedition/The Hollow Diamond.py
+++ Scientific Expedition/The Hollow Diamond.py
@@ -1,6 +1,7 @@
 from string import ascii_lowercase as alphabet
 from collections import deque
 
+
 def hollow_diamond(side: int, length: int, cw: bool) -> str:
 
     letters = deque(alphabet[:length] + (4 * side - 4 - length) * '*')
@@ -9,20 +10,36 @@ def hollow_diamond(side: int, length: int, cw: bool) -> str:
     inner = [1]
     for el in range(side - 2):
         inner.append(inner[-1] + 2)
-    inner +=  inner[:-1][::-1]
+    inner += inner[:-1][::-1]
     for i in range(len(outer)):
         if cw:
-            diamond += outer[i] * ' ' + letters.pop() + inner[i] * ' ' + letters.popleft() + '\n'
+            diamond += outer[i] * ' ' + letters.pop() + \
+                inner[i] * ' ' + letters.popleft() + '\n'
         else:
-            diamond += outer[i] * ' ' + letters.popleft() + inner[i] * ' ' + letters.pop() + '\n'
+            diamond += outer[i] * ' ' + letters.popleft() + \
+                inner[i] * ' ' + letters.pop() + '\n'
     diamond += (side - 1) * ' ' + letters.pop()
 
-    print(diamond)
+    return diamond
 
 
+print("Example:")
+print(hollow_diamond(3, 6, False))
 
+# These "asserts" are used for self-checking
+assert hollow_diamond(3, 8, True) == "  a\n h b\ng   c\n f d\n  e"
+assert hollow_diamond(3, 6, False) == "  a\n b *\nc   *\n d f\n  e"
+assert (
+    hollow_diamond(
+        4, 10, False) == "   a\n  b *\n c   *\nd     j\n e   i\n  f h\n   g"
+)
+assert (
+    hollow_diamond(5, 16, True)
+    == "    a\n   p b\n  o   c\n n     d\nm       e\n l     f\n  k   g\n   j h\n    i"
+)
+assert (
+    hollow_diamond(5, 14, False)
+    == "    a\n   b *\n  c   *\n d     n\ne       m\n f     l\n  g   k\n   h j\n    i"
+)
 
-hollow_diamond(3, 8, True) == "  a\n h b\ng   c\n f d\n  e"
-hollow_diamond(3, 6, False) == "  a\n b *\nc   *\n d f\n  e"
-hollow_diamond(4, 10, False) == "   a\n  b *\n c   *\nd     j\n e   i\n  f h\n   g"
-hollow_diamond(5, 16, True) == "    a\n   p b\n  o   c\n n     d\nm       e\n l     f\n  k   g\n   j h\n    i"
+print("The mission is done! Click 'Check Solution' to earn rewards!")
