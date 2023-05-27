7d8c4ca397eff390aee3ae66aeb89d073d7316e6
mal1on <lyubo.malinov@gmail.com>
2023-05-27 19:41:09 +0300

    Lightbulb End Watching - all checks successful

--- Master of The Time Stone/Lightbulb End Watching.py
+++ Master of The Time Stone/Lightbulb End Watching.py
@@ -2,18 +2,23 @@ from datetime import datetime
 from typing import List, Optional
 
 
-def sum_light(els: List[datetime], start_watching: Optional[datetime] = None) -> int:
+def sum_light(els: List[datetime], start_watching: Optional[datetime] = None, end_watching: Optional[datetime] = None,) -> int:
     """
     how long the light bulb has been turned on
     """
 
     new_els = []
     start_watching = els[0] if not start_watching else start_watching
+    end_watching = els[-1] if not end_watching else end_watching
+    if len(els) % 2:
+        els.append(end_watching)
     for on, off in zip(els[0::2], els[1::2]):
         if on <= start_watching and off > start_watching:
-            new_els.append([start_watching, off])
-        elif on > start_watching:
-            new_els.append((on, off))
+            new_els.append((start_watching, off))
+        elif on <= start_watching and off >= end_watching:
+            new_els.append((start_watching, end_watching))
+        elif end_watching > on > start_watching and off >= end_watching:
+            new_els.append((on, end_watching))
 
     print(sum([(off - on).total_seconds() for on, off in new_els]))
 
@@ -26,17 +31,17 @@ sum_light([
 datetime(2015, 1, 12, 10, 10, 0),
 datetime(2015, 1, 12, 11, 0, 10)) == 20
 
-# sum_light([
-#     datetime(2015, 1, 12, 10, 0, 0),
-# ],
-# datetime(2015, 1, 12, 9, 9, 0),
-# datetime(2015, 1, 12, 10, 0, 0)) == 0
+sum_light([
+    datetime(2015, 1, 12, 10, 0, 0),
+],
+datetime(2015, 1, 12, 9, 9, 0),
+datetime(2015, 1, 12, 10, 0, 0)) == 0
 
-# sum_light([
-#     datetime(2015, 1, 12, 10, 0, 0),
-#     datetime(2015, 1, 12, 10, 10, 10),
-#     datetime(2015, 1, 12, 11, 0, 0),
-#     datetime(2015, 1, 12, 11, 10, 10),
-# ],
-# datetime(2015, 1, 12, 9, 0, 0),
-# datetime(2015, 1, 12, 10, 5, 0)) == 300
+sum_light([
+    datetime(2015, 1, 12, 10, 0, 0),
+    datetime(2015, 1, 12, 10, 10, 10),
+    datetime(2015, 1, 12, 11, 0, 0),
+    datetime(2015, 1, 12, 11, 10, 10),
+],
+datetime(2015, 1, 12, 9, 0, 0),
+datetime(2015, 1, 12, 10, 5, 0)) == 300
