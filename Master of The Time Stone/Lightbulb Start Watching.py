b22bfd5374ce40b64fe7cd4a55d9013072300663
mal1on <lyubo.malinov@gmail.com>
2023-05-27 14:50:01 +0300

    Lightbulb Start Watching - mission completed

--- Master of The Time Stone/Lightbulb Start Watching.py
+++ Master of The Time Stone/Lightbulb Start Watching.py
@@ -8,32 +8,117 @@ def sum_light(els: List[datetime], start_watching: Optional[datetime] = None) ->
     """
 
     new_els = []
+    start_watching = els[0] if not start_watching else start_watching
     for on, off in zip(els[0::2], els[1::2]):
-        if (on <= start_watching and off > start_watching):
+        if on <= start_watching and off > start_watching:
             new_els.append([start_watching, off])
         elif on > start_watching:
-            new_els.append(on, off)
+            new_els.append((on, off))
 
-    print(sum([(off - on).total_seconds() for on, off in new_els]))
-
-
+    return sum([(off - on).total_seconds() for on, off in new_els])
 
 
+if __name__ == "__main__":
+    print("Example:")
+    print(
+        sum_light(
+            [
+                datetime(2015, 1, 12, 10, 0, 0),
+                datetime(2015, 1, 12, 10, 0, 10),
+            ],
+            datetime(2015, 1, 12, 10, 0, 5),
+        )
+    )
 
-sum_light([
-    datetime(2015, 1, 12, 10, 0, 0),
-    datetime(2015, 1, 12, 10, 0, 10),
-],
-datetime(2015, 1, 12, 10, 0, 5)) == 5
+    assert (
+        sum_light(
+            els=[
+                datetime(2015, 1, 12, 10, 0, 0),
+                datetime(2015, 1, 12, 10, 0, 10),
+            ],
+            start_watching=datetime(2015, 1, 12, 10, 0, 5),
+        )
+        == 5
+    )
 
-sum_light([
-    datetime(2015, 1, 12, 10, 0, 0),
-    datetime(2015, 1, 12, 10, 0, 10),
-], datetime(2015, 1, 12, 10, 0, 0)) == 10
+    assert (
+        sum_light(
+            [
+                datetime(2015, 1, 12, 10, 0, 0),
+                datetime(2015, 1, 12, 10, 0, 10),
+            ],
+            datetime(2015, 1, 12, 10, 0, 0),
+        )
+        == 10
+    )
 
-sum_light([
-    datetime(2015, 1, 12, 10, 0, 0),
-    datetime(2015, 1, 12, 10, 10, 10),
-    datetime(2015, 1, 12, 11, 0, 0),
-    datetime(2015, 1, 12, 11, 10, 10),
-], datetime(2015, 1, 12, 11, 0, 0)) == 610
+    assert (
+        sum_light(
+            [
+                datetime(2015, 1, 12, 10, 0, 0),
+                datetime(2015, 1, 12, 10, 10, 10),
+                datetime(2015, 1, 12, 11, 0, 0),
+                datetime(2015, 1, 12, 11, 10, 10),
+            ],
+            datetime(2015, 1, 12, 11, 0, 0),
+        )
+        == 610
+    )
+
+    assert (
+        sum_light(
+            [
+                datetime(2015, 1, 12, 10, 0, 0),
+                datetime(2015, 1, 12, 10, 10, 10),
+                datetime(2015, 1, 12, 11, 0, 0),
+                datetime(2015, 1, 12, 11, 10, 10),
+            ],
+            datetime(2015, 1, 12, 11, 0, 10),
+        )
+        == 600
+    )
+
+    assert (
+        sum_light(
+            [
+                datetime(2015, 1, 12, 10, 0, 0),
+                datetime(2015, 1, 12, 10, 10, 10),
+                datetime(2015, 1, 12, 11, 0, 0),
+                datetime(2015, 1, 12, 11, 10, 10),
+            ],
+            datetime(2015, 1, 12, 10, 10, 0),
+        )
+        == 620
+    )
+
+    assert (
+        sum_light(
+            [
+                datetime(2015, 1, 12, 10, 0, 0),
+                datetime(2015, 1, 12, 10, 10, 10),
+                datetime(2015, 1, 12, 11, 0, 0),
+                datetime(2015, 1, 12, 11, 10, 10),
+                datetime(2015, 1, 12, 11, 10, 11),
+                datetime(2015, 1, 12, 12, 10, 11),
+            ],
+            datetime(2015, 1, 12, 12, 10, 11),
+        )
+        == 0
+    )
+
+    assert (
+        sum_light(
+            [
+                datetime(2015, 1, 12, 10, 0, 0),
+                datetime(2015, 1, 12, 10, 10, 10),
+                datetime(2015, 1, 12, 11, 0, 0),
+                datetime(2015, 1, 12, 11, 10, 10),
+                datetime(2015, 1, 12, 11, 10, 11),
+                datetime(2015, 1, 12, 12, 10, 11),
+            ],
+            datetime(2015, 1, 12, 12, 9, 11),
+        )
+        == 60
+    )
+
+    print("The second mission in series is done? Click 'Check' to earn cool rewards!")
