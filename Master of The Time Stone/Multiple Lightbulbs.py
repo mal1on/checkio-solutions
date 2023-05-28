88d8d86b00ff81189b406414a2546c975bc806ee
mal1on <lyubo.malinov@gmail.com>
2023-05-28 18:02:47 +0300

    Multiple lightbulbs - all checks successful

--- Master of The Time Stone/Multiple Lightbulbs.py
+++ Master of The Time Stone/Multiple Lightbulbs.py
@@ -5,19 +5,13 @@ from itertools import groupby
 
 def sum_light(els: List[Union[datetime, Tuple[datetime, int]]], start_watching: Optional[datetime] = None, end_watching: Optional[datetime] = None,) -> int:
 
-    # new_els = []
-    # start_watching = els[0] if not start_watching else start_watching
-    # end_watching = els[-1] if not end_watching else end_watching
-    # els += [end_watching] if len(els) % 2 else []
-    # for on, off in zip(els[0::2], els[1::2]):
-    #     if end_watching > on and start_watching < off:
-    #         new_els.append((max(on, start_watching), min(off, end_watching)))
-
-    # print(sum([(off - on).total_seconds() for on, off in new_els]))
-
     for i, el in enumerate(els):
         if isinstance(el, datetime):
             els[i] = (el, 1)
+
+    start_watching = els[0][0] if not start_watching else start_watching
+    end_watching = els[-1][0] if not end_watching else end_watching
+    els += [end_watching] if len(els) % 2 else []
 
     bulbs = {k: [d[0] for d in list(g)] for k, g in groupby(
         sorted(els, key=lambda el: el[1]), key=lambda el: el[1])}
@@ -27,27 +21,29 @@ def sum_light(els: List[Union[datetime, Tuple[datetime, int]]], start_watching: 
     for l in [list(zip(bulbs[k][0::2], bulbs[k][1::2])) for k in bulbs]:
         lighted += l
 
-    new_els = [sorted(lighted, key=lambda t: t[0])[0]]
-    for on, off in sorted(lighted, key=lambda t: t[0])[1:]:
+    lighted.sort(key=lambda t: t[0])
+    new_els = [lighted[0]]
+    for on, off in lighted[1:]:
         if on <= new_els[-1][1] < off:
             new_els[-1] = (new_els[-1][0], off)
         else:
             new_els.append((on, off))
 
-    print(new_els)
+    result = []
+
+    for on, off in new_els:
+        if end_watching > on and start_watching < off:
+            result.append((max(on, start_watching), min(off, end_watching)))
+
+    print(sum([(off - on).total_seconds() for on, off in result]))
 
 
-
-
-
-
-
-# sum_light([
-#     datetime(2015, 1, 12, 10, 0, 0),
-#     datetime(2015, 1, 12, 10, 0, 10),
-#     (datetime(2015, 1, 12, 10, 0, 0), 2),
-#     (datetime(2015, 1, 12, 10, 1, 0), 2),
-# ]) == 60
+sum_light([
+    datetime(2015, 1, 12, 10, 0, 0),
+    datetime(2015, 1, 12, 10, 0, 10),
+    (datetime(2015, 1, 12, 10, 0, 0), 2),
+    (datetime(2015, 1, 12, 10, 1, 0), 2),
+]) == 60
 
 
 sum_light([
@@ -61,16 +57,16 @@ sum_light([
     (datetime(2015, 1, 12, 10, 1, 20), 3),
 ]) == 60
 
-# sum_light([
-#     datetime(2015, 1, 12, 10, 0, 20),
-#     (datetime(2015, 1, 12, 10, 0, 30), 2),
-#     datetime(2015, 1, 12, 10, 0, 40),
-#     (datetime(2015, 1, 12, 10, 0, 50), 2),
-# ], datetime(2015, 1, 12, 10, 0, 30)) == 20
+sum_light([
+    datetime(2015, 1, 12, 10, 0, 20),
+    (datetime(2015, 1, 12, 10, 0, 30), 2),
+    datetime(2015, 1, 12, 10, 0, 40),
+    (datetime(2015, 1, 12, 10, 0, 50), 2),
+], datetime(2015, 1, 12, 10, 0, 30)) == 20
 
-# sum_light([
-#     datetime(2015, 1, 12, 10, 0, 0),
-#     datetime(2015, 1, 12, 10, 0, 10),
-#     (datetime(2015, 1, 12, 10, 0, 0), 2),
-#     (datetime(2015, 1, 12, 10, 1, 0), 2),
-# ], datetime(2015, 1, 12, 10, 0, 20), datetime(2015, 1, 12, 10, 1, 0)) == 40
+sum_light([
+    datetime(2015, 1, 12, 10, 0, 0),
+    datetime(2015, 1, 12, 10, 0, 10),
+    (datetime(2015, 1, 12, 10, 0, 0), 2),
+    (datetime(2015, 1, 12, 10, 1, 0), 2),
+], datetime(2015, 1, 12, 10, 0, 20), datetime(2015, 1, 12, 10, 1, 0)) == 40
