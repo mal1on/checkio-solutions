32cc6bd50d42e909940bf0c1c4416da411a6d8aa
mal1on <lyubo.malinov@gmail.com>
2023-05-29 16:39:28 +0300

    Time Converter (24h to 12h) - mission completed

--- Master of The Time Stone/Time Converter (24h to 12h).py
+++ Master of The Time Stone/Time Converter (24h to 12h).py
@@ -1,10 +1,15 @@
 from datetime import datetime as dt
 
+
 def time_converter(time: str) -> str:
-    # replace this for solution
-    print(dt.strptime(time, '%H:%M').strftime('%I:%M %p').replace('PM', 'p.m.').replace('AM', 'a.m.').lstrip('0'))
+
+    return dt.strptime(time, '%H:%M').strftime('%I:%M %p').replace('PM', 'p.m.').replace('AM', 'a.m.').lstrip('0')
 
 
+print("Example:")
+print(time_converter("12:30"))
 
-time_converter("12:30") == "12:30 p.m."
-time_converter("09:00") == "9:00 a.m."
+assert time_converter("12:30") == "12:30 p.m."
+assert time_converter("09:00") == "9:00 a.m."
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
