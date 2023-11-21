80e1d0129f391de72c31a469ab741be643ce0bcf
mal1on <lyubo.malinov@gmail.com>
2023-11-21 12:57:30 +0200

    Most Wanted Letter - first working solution

--- Polygon/Most Wanted Letter.py
+++ Polygon/Most Wanted Letter.py
@@ -1,12 +1,18 @@
-def say_hi(name, age):
+from collections import Counter
 
-    return f"Hi. My name is {name} and I'm {age} years old"
+def most_wanted(text: str) -> str:
+
+    c = Counter(char for char in text.lower() if char.isalpha())
+    max_count = c.most_common(1)[0][1]
+    print([el for el in c if c[el] == max_count])
 
 
-if __name__ == '__main__':
-    # These "asserts" using only for self-checking and not necessary for auto-testing
-    assert say_hi(
-        "Alex", 32) == "Hi. My name is Alex and I'm 32 years old", "First"
-    assert say_hi(
-        "Frank", 68) == "Hi. My name is Frank and I'm 68 years old", "Second"
-    print('Done. Time to Check.')
+
+
+
+most_wanted("Hello World!") == ["l"]
+most_wanted("How do you do?") == ["o"]
+most_wanted("One") == ["o", "n", "e"]
+most_wanted("Oops!") == ["o"]
+most_wanted("AAaooo!!!!") == ["a", "o"]
+most_wanted("abe") == ["a", "b", "e"]
