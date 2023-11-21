3c7aa9ee488a02d6ae624459cdbcfecc51a7f9ec
mal1on <lyubo.malinov@gmail.com>
2023-11-21 12:58:53 +0200

    Most Wanted Letter - asserts successful

--- Polygon/Most Wanted Letter.py
+++ Polygon/Most Wanted Letter.py
@@ -4,15 +4,17 @@ def most_wanted(text: str) -> str:
 
     c = Counter(char for char in text.lower() if char.isalpha())
     max_count = c.most_common(1)[0][1]
-    print([el for el in c if c[el] == max_count])
+    return [el for el in c if c[el] == max_count]
 
 
-
-
-
-most_wanted("Hello World!") == ["l"]
-most_wanted("How do you do?") == ["o"]
-most_wanted("One") == ["o", "n", "e"]
-most_wanted("Oops!") == ["o"]
-most_wanted("AAaooo!!!!") == ["a", "o"]
-most_wanted("abe") == ["a", "b", "e"]
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert sorted(most_wanted("Hello World!")) == ["l"], "Hello test"
+    assert sorted(most_wanted("How do you do?")) == ["o"], "O is most wanted"
+    assert sorted(most_wanted("One")) == ["e", "n", "o"], "All letter only once."
+    assert sorted(most_wanted("Oops!")) == ["o"], "Don't forget about lower case."
+    assert sorted(most_wanted("AAaooo!!!!")) == ["a", "o"], "Only letters."
+    assert sorted(most_wanted("abe")) == ["a", "b", "e"], "The First."
+    print("Start the long test")
+    assert sorted(most_wanted("a" * 9000 + "b" * 1000)) == ["a"], "Long."
+    print("The local tests are done.")
