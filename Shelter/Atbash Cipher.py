5f8d72901e4c190cb95ed2a67fd7dfe9f187d6b9
mal1on <lyubo.malinov@gmail.com>
2023-11-24 15:37:20 +0200

    Atbash Cipher - checks successful

--- Shelter/Atbash Cipher.py
+++ Shelter/Atbash Cipher.py
@@ -1,6 +1,18 @@
+from string import ascii_lowercase as low, ascii_uppercase as upp
+
 def atbash(plaintext: str) -> str:
-    # your code here
-    return ""
+
+    result = ''
+
+    for ch in plaintext:
+        if ch in low:
+            result += low[- low.index(ch) - 1]
+        elif ch in upp:
+            result += upp[- upp.index(ch) - 1]
+        else:
+            result += ch
+
+    print(result)
 
 
 
