50cc091aacf3158f7283ec2203c8cb5ac6aef9d3
mal1on <lyubo.malinov@gmail.com>
2023-11-27 11:13:15 +0200

    Scytale Encryption - mission completed

--- Shelter/Scytale Encryption.py
+++ Shelter/Scytale Encryption.py
@@ -1,28 +1,40 @@
 from itertools import zip_longest
 
+
 def scytale_decipher(ciphertext, crib):
 
     results = []
 
-    for c in range(1, len(crib) + 1):
+    for c in range(1, len(ciphertext)):
 
         groups = []
 
         for i in range(0, len(ciphertext), c):
-            groups.append(ciphertext[i:i+c])
+            groups.append(ciphertext[i:i + c])
 
-        decoded = ''.join([''.join(g) for g in list(zip_longest(*groups, fillvalue ='_'))])
+        decoded = ''.join([''.join(g)
+                           for g in list(zip_longest(*groups, fillvalue='_'))])
 
-        if crib in decoded:
-            results.append(decoded.rstrip('_'))
+        if crib in decoded.replace('_', ''):
+            results.append(decoded.replace('_', ''))
 
-
-    print(results[0] if len(results) == 1 else None)
-
-
+    return results[0] if len(results) == 1 else None
 
 
-scytale_decipher('aaaatctwtkdn', 'dawn') == 'attackatdawn'
-scytale_decipher('hdoeerlallrdow', 'world') == 'hellodearworld'
-scytale_decipher('totetshpmeecisendysescwticsriasraytlaegphet', 'sicret') == None     #Crib is not in plaintext
-scytale_decipher('aaaatctwtkdn', 'at') == None                                        #More than one possible decryptions
+if __name__ == "__main__":
+    print("Example:")
+    print(scytale_decipher("aaaatctwtkdn", "dawn"))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert scytale_decipher("aaaatctwtkdn", "dawn") == "attackatdawn"
+    assert scytale_decipher("hdoeerlallrdow", "world") == "hellodearworld"
+    assert (
+        scytale_decipher(
+            "totetshpmeecisendysescwticsriasraytlaegphet", "sicret")
+        == None
+    ), "Crib is not in plaintext"
+    assert (
+        scytale_decipher("aaaatctwtkdn", "at") == None
+    ), "More than one possible decryptions"
+
+    print("Coding complete? Click 'Check' to earn cool rewards!")
