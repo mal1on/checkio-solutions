f8482449bccbd32e41afdfc4613b9553456308b9
mal1on <lyubo.malinov@gmail.com>
2023-11-27 10:44:56 +0200

    Scytale Encryption - checks successful

--- Shelter/Scytale Encryption.py
+++ Shelter/Scytale Encryption.py
@@ -1,9 +1,23 @@
-from typing import Optional
+from itertools import zip_longest
+
+def scytale_decipher(ciphertext, crib):
+
+    results = []
+
+    for c in range(1, len(crib) + 1):
+
+        groups = []
+
+        for i in range(0, len(ciphertext), c):
+            groups.append(ciphertext[i:i+c])
+
+        decoded = ''.join([''.join(g) for g in list(zip_longest(*groups, fillvalue ='_'))])
+
+        if crib in decoded:
+            results.append(decoded.rstrip('_'))
 
 
-def scytale_decipher(ciphertext: str, crib: str) -> Optional[str]:
-    # your code here
-    return ""
+    print(results[0] if len(results) == 1 else None)
 
 
 
