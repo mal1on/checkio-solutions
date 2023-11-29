8ad9f21d89f1c537370935daa2bf48aad0f589d7
mal1on <lyubo.malinov@gmail.com>
2023-11-29 12:03:33 +0200

    Shelter - Remove Accents - mission completed

--- Shelter/Remove Accents.py
+++ Shelter/Remove Accents.py
@@ -1,11 +1,12 @@
 import unicodedata
 
+
 def checkio(in_string):
 
-   return ''.join(c for c in unicodedata.normalize('NFD', in_string) if unicodedata.category(c) != 'Mn')
+    return ''.join(c for c in unicodedata.normalize('NFD', in_string) if unicodedata.category(c) != 'Mn')
 
 
-
-checkio(u"préfèrent") == u"preferent"
-checkio(u"loài trăn lớn") == u"loai tran lon"
-checkio("完好無缺")
+if __name__ == '__main__':
+    assert checkio(u"préfèrent") == u"preferent"
+    assert checkio(u"loài trăn lớn") == u"loai tran lon"
+    print('Done')
