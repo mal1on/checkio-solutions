0840ab9b1a06e80ed29a082438eded4e232375be
mal1on <lyubo.malinov@gmail.com>
2023-12-03 11:52:18 +0200

    Blizzard - English to Braille Translator - all braille rows correct

--- Blizzard/English to Braille Translator,py.py
+++ Blizzard/English to Braille Translator,py.py
@@ -60,23 +60,53 @@ from string import ascii_lowercase, ascii_uppercase
 
 def braille_page(text: str) -> list[list[int]]:
 
-    result = []
+    brailles = []
 
     for ch in text:
         if ch in ascii_lowercase:
-            result.append(LETTERS_NUMBERS[ord(ch) - 97])
+            brailles.append(LETTERS_NUMBERS[ord(ch) - 97])
         elif ch in ascii_uppercase:
-            result.append([CAPITAL_FORMAT] + [LETTERS_NUMBERS[ord(ch) - 97]])
+            brailles.append(CAPITAL_FORMAT)
+            brailles.append(LETTERS_NUMBERS[ord(ch) - 65])
         elif ch.isdigit():
-            result.append([NUMBER_FORMAT] + [LETTERS_NUMBERS[ord(ch) - 49]])
+            brailles.append(NUMBER_FORMAT)
+            brailles.append(LETTERS_NUMBERS[ord(ch) - 49])
         elif ch in PUNCTUATION.keys():
-            result.append(PUNCTUATION[ch])
+            brailles.append(PUNCTUATION[ch])
         elif ch == ' ':
-            result.append([[0, 0], [0, 0], [0, 0]])
+            brailles.append([[0, 0], [0, 0], [0, 0]])
+
+    # result = [[e[0], e[1], 0] for b in list(zip(*brailles)) for e in b]
+
+    result = []
+    row = []
+    count = 0
+
+    for b in list(zip(*brailles)):
+        for e in b:
+            if count < 9:
+                temp = []
+                temp.append(e[0])
+                temp.append(e[1])
+                temp.append(0)
+                row.extend(temp)
+                count += 1
+            else:
+                temp = []
+                temp.append(e[0])
+                temp.append(e[1])
+                row.extend(temp)
+                result.append(row)
+                result.extend([[0] * 29])
+
+                count = 0
+                row = []
+
+    print(result)
 
     return result
 
 
 
 
-print(braille_page('!'))
+print(braille_page('hello 1st World!'))
