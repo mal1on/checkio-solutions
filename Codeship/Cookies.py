23053126c9791e26ccfbe037bc47f56e1760a629
mal1on <lyubo.malinov@gmail.com>
2023-12-06 15:36:21 +0200

    Codeship - Cookies - mission completed

--- Codeship/Cookies.py
+++ Codeship/Cookies.py
@@ -1,8 +1,17 @@
 def get_cookie(cookie, name):
-    return "value"
+
+    for chunk in cookie.split(';'):
+        if name in chunk:
+            return chunk[chunk.find(name) + len(name) + 1:]
 
 
-
-
-get_cookie('theme=light; sessionToken=abc123', 'theme') == 'light'
-get_cookie('_ga=GA1.2.447610749.1465220820; _gat=1; ffo=true', 'ffo') == 'true'
+if __name__ == "__main__":
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert (
+        get_cookie("theme=light; sessionToken=abc123", "theme") == "light"
+    ), "theme=light"
+    assert (
+        get_cookie(
+            "_ga=GA1.2.447610749.1465220820; _gat=1; ffo=true", "ffo") == "true"
+    ), "ffo=true"
+    print("Looks like you know everything. It is time for 'Check'!")
