92fa8f18bbe49dca1e3e9052aad8e82cfffcfe11
mal1on <lyubo.malinov@gmail.com>
2023-12-06 09:39:17 +0200

    Blizzard - Identify Block - all blocks identified

--- Blizzard/Identify Block.py
+++ Blizzard/Identify Block.py
@@ -6,8 +6,17 @@ def identify_block(numbers):
     order = sorted(numbers)
     if diff == 3 or diff == 12:
         result = 'I'
-    elif order[1] - order[0] == 1 and order[3] - order[2] == 1 and order[2] - order[1] > 1:
-        result = 'O'
+    elif order[1] - order[0] == 1 and order[3] - order[2] == 1:
+        if diff == 5:
+            result = 'O'
+        elif diff == 4:
+            result = 'S'
+        elif diff == 6:
+            result = 'Z'
+    elif order[1] - order[0] == 4 and order[3] - order[2] == 4 and diff == 9:
+        result = 'S'
+    elif order[1] - order[0] == 3 and order[3] - order[2] == 3 and diff == 7:
+        result = 'Z'
     elif order[2] - order[0] == 2:
         base = order[:2]
         if max(numbers) - base[1] == 4:
@@ -45,3 +54,4 @@ identify_block({1, 5, 6, 10}) == 'S'
 identify_block({10, 13, 14, 15}) == 'T', 'T'
 identify_block({1, 5, 9, 6}) == 'T', 'T'
 identify_block({2, 3, 7, 11}) == 'L', 'L'
+identify_block({1, 2, 6, 7}) == 'Z', 'Z'
