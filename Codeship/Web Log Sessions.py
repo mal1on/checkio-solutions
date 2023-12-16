aea5314031a6dff18ac9b3616e16f5873dd54147
mal1on <lyubo.malinov@gmail.com>
2023-12-16 17:18:17 +0200

    Codeship - Web Log Sessions - mission completed

--- Codeship/Web Log Sessions.py
+++ Codeship/Web Log Sessions.py
@@ -2,10 +2,11 @@ import re
 from datetime import datetime
 from operator import itemgetter
 
+
 def checkio(log_text):
 
     lines = log_text.splitlines()
-    session, sessions, session_data, result = [], [], [], []
+    session, session_data, result = [], [], []
     session_time = 1
     pattern = '(https?://)(\w+\.)?(\w+\.\w+)'
 
@@ -14,8 +15,6 @@ def checkio(log_text):
         t, u, a = session[0].lower().split(';;')
         t = datetime.strptime(t, '%Y-%m-%d-%H-%M-%S')
         d = re.search(pattern, a).group(3)
-        session_data.append(u)
-        session_data.append(d)
         for line in lines:
             t_l, u_l, a_l = line.lower().split(';;')
             t_l = datetime.strptime(t_l, '%Y-%m-%d-%H-%M-%S')
@@ -24,23 +23,30 @@ def checkio(log_text):
                 session.append(line)
                 session_time += int((t_l - t).total_seconds())
                 t = t_l
-        session_data.append(session_time)
-        session_data.append(len(session))
+        [session_data.append(el) for el in [u, d, session_time, len(session)]]
         result.append(session_data)
-        sessions.append(session)
         lines = [l for l in lines if l not in session]
         session, session_data = [], []
         session_time = 1
 
     result = sorted(result, key=itemgetter(0, 1, 2))
     result = '\n'.join([';;'.join(list(map(str, l))) for l in result])
-    print(result)
+
+    return result
 
 
-checkio("""2013-01-01-01-00-00;;Name;;http://checkio.org/task
+# These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert (checkio(
+        """2013-01-01-01-00-00;;Name;;http://checkio.org/task
 2013-01-01-01-02-00;;name;;http://checkio.org/task2
 2013-01-01-01-31-00;;Name;;https://admin.checkio.org
 2013-01-01-03-00-00;;Name;;http://www.checkio.org/profile
 2013-01-01-03-00-01;;Name;;http://example.com
 2013-02-03-04-00-00;;user2;;http://checkio.org/task
 2013-01-01-03-11-00;;Name;;http://checkio.org/task""")
+        ==
+        """name;;checkio.org;;661;;2
+name;;checkio.org;;1861;;3
+name;;example.com;;1;;1
+user2;;checkio.org;;1;;1"""), "Example"
