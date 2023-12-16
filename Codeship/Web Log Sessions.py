90934cda911a42d105725931bd7c7f2544df467d
mal1on <lyubo.malinov@gmail.com>
2023-12-16 15:11:23 +0200

    Codeship - Web Log Sessions - result available in not sorted list

--- Codeship/Web Log Sessions.py
+++ Codeship/Web Log Sessions.py
@@ -3,8 +3,10 @@ from datetime import datetime
 
 def checkio(log_text):
 
-    lines = sorted(log_text.splitlines(), key=lambda l: l.split(';;')[0])
-    session, sessions = [], []
+    lines = log_text.splitlines()
+    session, sessions, result = [], [], []
+    session_data = ''
+    session_time = 1
     pattern = '(https?://)(\w+\.)?(\w+\.\w+)'
 
     while lines:
@@ -12,21 +14,25 @@ def checkio(log_text):
         t, u, a = session[0].lower().split(';;')
         t = datetime.strptime(t, '%Y-%m-%d-%H-%M-%S')
         d = re.search(pattern, a).group(3)
+        session_data += (u + ';;' + d + ';;')
         for line in lines:
             t_l, u_l, a_l = line.lower().split(';;')
             t_l = datetime.strptime(t_l, '%Y-%m-%d-%H-%M-%S')
             d_l = re.search(pattern, a_l).group(3)
             if (t_l - t).total_seconds() <= 1800 and u_l == u and d == d_l:
                 session.append(line)
+                session_time += (t_l - t).total_seconds()
                 t = t_l
+        session_data += (str(session_time) + ';;')
+        session_data += str(len(session))
+        result.append(session_data)
         sessions.append(session)
         lines = [l for l in lines if l not in session]
         session = []
+        session_data = ''
+        session_time = 1
 
-    for s in sessions:
-        print(s)
-
-
+    print(result)
 
 
 checkio("""2013-01-01-01-00-00;;Name;;http://checkio.org/task
