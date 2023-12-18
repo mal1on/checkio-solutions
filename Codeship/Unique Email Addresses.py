1f255ebd5302735265e8425be49bf547d4bf5567
mal1on <lyubo.malinov@gmail.com>
2023-12-18 21:57:06 +0200

    Codeship - Unique Email Addresses - all checks successful

--- Codeship/Unique Email Addresses.py
+++ Codeship/Unique Email Addresses.py
@@ -1,8 +1,10 @@
 def unique_emails(emails: list[str]) -> int:
 
-    return 0
-
+    return len(set([''.join([l for l in mail.split('@')[0] if l != '.']).split('+')[0] for mail in emails]))
 
 
 unique_emails(["alex@checkio.org", "mike@google.com", "lili@apple.com"])
-# unique_emails(["mi.ke@google.com", "alex@checkio.org", "mike@google.com", "lili@apple.com"])
+unique_emails(["mi.ke@google.com", "alex@checkio.org",
+               "mike@google.com", "lili@apple.com"])
+unique_emails(["alex+home@checkio.org", "lili+work@apple.com",
+               "alex@checkio.org", "lili@apple.com", ])
