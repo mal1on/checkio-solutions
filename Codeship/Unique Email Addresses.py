b11d7dc0840e2a0c5c011907becf8c87cfb04e72
mal1on <lyubo.malinov@gmail.com>
2023-12-18 21:47:10 +0200

    Codeship - Unique Email Addresses - mission file created

---
+++ Codeship/Unique Email Addresses.py
@@ -0,0 +1,8 @@
+def unique_emails(emails: list[str]) -> int:
+
+    return 0
+
+
+
+unique_emails(["alex@checkio.org", "mike@google.com", "lili@apple.com"])
+# unique_emails(["mi.ke@google.com", "alex@checkio.org", "mike@google.com", "lili@apple.com"])
