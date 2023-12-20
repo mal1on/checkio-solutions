391e4198d18d7726c770d5d05eeac459a7f88cc4
mal1on <lyubo.malinov@gmail.com>
2023-12-20 13:47:01 +0200

    Codeship - Backspace Apply - mission file created

---
+++ Codeship/Backspace Apply.py
@@ -0,0 +1,12 @@
+def backspace_apply(val: str) -> str:
+
+
+
+    return val
+
+
+
+backspace_apply("name") == "name"
+# backspace_apply("name#") == "nam"
+# backspace_apply("na##me") == "me"
+# backspace_apply("nam#e#") == "na"
