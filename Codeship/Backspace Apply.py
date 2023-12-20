0626cc711261a59abec6fc9085830d50d8a8990b
mal1on <lyubo.malinov@gmail.com>
2023-12-20 13:56:49 +0200

    Codeship - Backspace Apply - all checks successful

--- Codeship/Backspace Apply.py
+++ Codeship/Backspace Apply.py
@@ -1,12 +1,22 @@
 def backspace_apply(val: str) -> str:
 
+    result = []
 
+    for ch in val:
+        if ch == '#':
+            if result:
+                result.pop()
+                continue
+            else:
+                continue
+        result.append(ch)
 
-    return val
+    print(''.join(result))
+
 
 
 
 backspace_apply("name") == "name"
-# backspace_apply("name#") == "nam"
-# backspace_apply("na##me") == "me"
-# backspace_apply("nam#e#") == "na"
+backspace_apply("name#") == "nam"
+backspace_apply("na##me") == "me"
+backspace_apply("nam#e#") == "na"
