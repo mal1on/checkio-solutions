d37dfe81d86f43db1f066c81d2dfe62d30ca70a7
mal1on <lyubo.malinov@gmail.com>
2023-12-20 14:01:32 +0200

    Codeship - Backspace Apply - mission completed

--- Codeship/Backspace Apply.py
+++ Codeship/Backspace Apply.py
@@ -11,12 +11,24 @@ def backspace_apply(val: str) -> str:
                 continue
         result.append(ch)
 
-    print(''.join(result))
+    return ''.join(result)
+
+
+print("Example:")
+print(backspace_apply("thna#m##e"))
+
+assert backspace_apply("name") == "name"
+assert backspace_apply("name#") == "nam"
+assert backspace_apply("na##me") == "me"
+assert backspace_apply("nam#e#") == "na"
+assert backspace_apply("##name") == "name"
+assert backspace_apply("name######") == ""
+assert backspace_apply("nam######e") == "e"
+assert backspace_apply("n###ame") == "ame"
+assert backspace_apply("thna#m##e") == "the"
+assert backspace_apply("oppo##r##t##u###nity") == "nity"
+
+print("Not bad! Click 'Check' to earn cool rewards!")
 
 
 
-
-backspace_apply("name") == "name"
-backspace_apply("name#") == "nam"
-backspace_apply("na##me") == "me"
-backspace_apply("nam#e#") == "na"
