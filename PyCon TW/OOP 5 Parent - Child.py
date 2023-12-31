a150ec01b3f626a7d4de21bb0ea35daae80296bb
mal1on <lyubo.malinov@gmail.com>
2023-12-31 17:09:48 +0200

    OOP 5 - Parent - Child - mission completed

--- PyCon TW/OOP 5 Parent - Child.py
+++ PyCon TW/OOP 5 Parent - Child.py
@@ -20,11 +20,20 @@ class Car:
         print('Engine has stopped')
 
 
+class ElectricCar(Car):
+
+    def __init__(self, battery_capacity, brand='', model=''):
+        super().__init__(brand, model)
+        self.battery_capacity = battery_capacity
+
+
 my_car = Car()
 some_car1 = Car('Ford', 'Mustang')
 some_car2 = Car(model='Camaro')
 
-print(some_car2.working_engine)
-
 some_car1.start_engine()
 some_car2.start_engine()
+
+my_electric_car = ElectricCar(100, brand='Tesla', model='Model 3')
+
+print(my_electric_car.brand)
