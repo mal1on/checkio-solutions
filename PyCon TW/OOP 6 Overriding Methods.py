2aba4feff72417118dc813fba1b1be6acf1c47a8
mal1on <lyubo.malinov@gmail.com>
2024-01-01 13:11:03 +0200

    OOP 6: Overriding Methods - mission completed

--- PyCon TW/OOP 6 Overriding Methods.py
+++ PyCon TW/OOP 6 Overriding Methods.py
@@ -26,6 +26,16 @@ class ElectricCar(Car):
         super().__init__(brand, model)
         self.battery_capacity = battery_capacity
 
+    def start_engine(self):
+
+        self.working_engine = True
+        print('Electric motor has started')
+
+    def stop_engine(self):
+
+        self.working_engine = False
+        print('Electric motor has stopped')
+
 
 my_car = Car()
 some_car1 = Car('Ford', 'Mustang')
@@ -36,4 +46,9 @@ some_car2.start_engine()
 
 my_electric_car = ElectricCar(100, brand='Tesla', model='Model 3')
 
-print(my_electric_car.brand)
+my_electric_car.start_engine()
+
+my_electric_car2 = ElectricCar(60, 'Toyota', 'Prius')
+
+my_electric_car2.start_engine()
+my_electric_car2.stop_engine()
