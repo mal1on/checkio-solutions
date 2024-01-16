f0b6063c84f7fcc223614e7314c5964a9112d367
mal1on <lyubo.malinov@gmail.com>
2024-01-16 23:39:24 +0200

    Incinerator - Create Intervals - all checks successful

--- Incinerator/Create Intervals.py
+++ Incinerator/Create Intervals.py
@@ -2,8 +2,23 @@ def create_intervals(data):
     """
     Create a list of intervals out of set of ints.
     """
-    # your code here
-    return None
+    data = sorted(data)
+    prev = data[0]
+    temp = [prev]
+    ints = []
+
+    for el in data[1:]:
+        if el - prev == 1:
+            temp.append(el)
+        else:
+            ints.append(temp)
+            temp = [el]
+        prev = el
+    ints.append(temp)
+    result = [(i[0], i[-1]) for i in ints]
+    print(result)
+
+
 
 
 create_intervals({1, 2, 3, 4, 5, 7, 8, 12}) == [(1, 5), (7, 8), (12, 12)]
