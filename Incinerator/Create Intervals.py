9a2f729e7aab589544bd2bacb34bd54a062db5d3
mal1on <lyubo.malinov@gmail.com>
2024-01-16 23:54:05 +0200

    Incinerator - Create Intervals - mission completed

--- Incinerator/Create Intervals.py
+++ Incinerator/Create Intervals.py
@@ -2,10 +2,13 @@ def create_intervals(data):
     """
     Create a list of intervals out of set of ints.
     """
-    data = sorted(data)
-    prev = data[0]
-    temp = [prev]
-    ints = []
+    if data:
+        data = sorted(data)
+        prev = data[0]
+        temp = [prev]
+        ints = []
+    else:
+        return []
 
     for el in data[1:]:
         if el - prev == 1:
@@ -15,11 +18,16 @@ def create_intervals(data):
             temp = [el]
         prev = el
     ints.append(temp)
-    result = [(i[0], i[-1]) for i in ints]
-    print(result)
+
+    return [(i[0], i[-1]) for i in ints]
 
 
-
-
-create_intervals({1, 2, 3, 4, 5, 7, 8, 12}) == [(1, 5), (7, 8), (12, 12)]
-create_intervals({1, 2, 3, 6, 7, 8, 4, 5}) == [(1, 8)]
+if __name__ == "__main__":
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert create_intervals({1, 2, 3, 4, 5, 7, 8, 12}) == [
+        (1, 5),
+        (7, 8),
+        (12, 12),
+    ], "First"
+    assert create_intervals({1, 2, 3, 6, 7, 8, 4, 5}) == [(1, 8)], "Second"
+    print("Almost done! The only thing left to do is to Check it!")
