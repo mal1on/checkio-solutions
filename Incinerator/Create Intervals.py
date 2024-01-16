3684c4e0c1c837036215fdbab396edd7f0cb45c7
mal1on <lyubo.malinov@gmail.com>
2024-01-16 23:25:57 +0200

    Incinerator - Create Intervals - mission file created

---
+++ Incinerator/Create Intervals.py
@@ -0,0 +1,10 @@
+def create_intervals(data):
+    """
+    Create a list of intervals out of set of ints.
+    """
+    # your code here
+    return None
+
+
+create_intervals({1, 2, 3, 4, 5, 7, 8, 12}) == [(1, 5), (7, 8), (12, 12)]
+create_intervals({1, 2, 3, 6, 7, 8, 4, 5}) == [(1, 8)]
