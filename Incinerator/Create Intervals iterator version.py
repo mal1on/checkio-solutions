2b5b9c9f58392f00d3ef1a0040b962c51531678d
mal1on <lyubo.malinov@gmail.com>
2024-01-17 21:16:44 +0200

    Create Intervals (iterator version) - all checks successful

--- Incinerator/Create Intervals iterator version.py
+++ Incinerator/Create Intervals iterator version.py
@@ -6,10 +6,10 @@ def create_intervals(data):
     lefts = [l for l in data if l - 1 not in data]
     rights = [r for r in data if r + 1 not in data]
 
-    return list(zip(lefts, rights))
+    return iter(zip(sorted(lefts), sorted(rights)))
 
 
 list(create_intervals(iter(sorted(list({1, 2, 3, 4, 5, 7, 8, 12}))))) == [
-                                      (1, 5), (7, 8), (12, 12)]
+    (1, 5), (7, 8), (12, 12)]
 list(create_intervals(iter(sorted(list({1, 2, 3, 6, 7, 8, 4, 5}))))) == [
-                                      (1, 8)]
+    (1, 8)]
