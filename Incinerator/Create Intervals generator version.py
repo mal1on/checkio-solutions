60df097c7622e58ecbacea50710cce03045271e5
mal1on <lyubo.malinov@gmail.com>
2024-01-17 22:08:08 +0200

    Create Intervals (generator version) - mission completed

--- Incinerator/Create Intervals generator version.py
+++ Incinerator/Create Intervals generator version.py
@@ -10,5 +10,13 @@ def create_intervals(data):
         yield i
 
 
-print(list(create_intervals({1, 2, 3, 4, 5, 7, 8, 12}))) == [(1, 5), (7, 8), (12, 12)]
-print(list(create_intervals({1, 2, 3, 6, 7, 8, 4, 5}))) == [(1, 8)]
+if __name__ == "__main__":
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert list(create_intervals({1, 2, 3, 4, 5, 7, 8, 12})) == [
+        (1, 5),
+        (7, 8),
+        (12, 12),
+    ], "First"
+    assert list(create_intervals({1, 2, 3, 6, 7, 8, 4, 5})) == [
+        (1, 8)], "Second"
+    print("Almost done! The only thing left to do is to Check it!")
