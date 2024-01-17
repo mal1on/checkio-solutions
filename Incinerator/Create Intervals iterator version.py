2ccb98353866eaeff36bffd6d17eb3431a636b5f
mal1on <lyubo.malinov@gmail.com>
2024-01-17 21:26:36 +0200

    Create Intervals (iterator version) - mission completed

--- Incinerator/Create Intervals iterator version.py
+++ Incinerator/Create Intervals iterator version.py
@@ -3,13 +3,24 @@ def create_intervals(data):
     Create a list of intervals out of set of ints.
     """
 
+    data = list(data)
+
     lefts = [l for l in data if l - 1 not in data]
     rights = [r for r in data if r + 1 not in data]
 
     return iter(zip(sorted(lefts), sorted(rights)))
 
 
-list(create_intervals(iter(sorted(list({1, 2, 3, 4, 5, 7, 8, 12}))))) == [
-    (1, 5), (7, 8), (12, 12)]
-list(create_intervals(iter(sorted(list({1, 2, 3, 6, 7, 8, 4, 5}))))) == [
-    (1, 8)]
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    res = create_intervals(iter(sorted(list({1, 2, 3, 4, 5, 7, 8, 12}))))
+    assert hasattr(
+        res, '__iter__'), "your function should return the iterator object"
+    assert hasattr(
+        res, '__next__'), "your function should return the iterator object"
+
+    assert list(create_intervals(iter(sorted(list({1, 2, 3, 4, 5, 7, 8, 12}))))) == [
+        (1, 5), (7, 8), (12, 12)], "First"
+    assert list(create_intervals(iter(sorted(list({1, 2, 3, 6, 7, 8, 4, 5}))))) == [
+        (1, 8)], "Second"
+    print('Almost done! The only thing left to do is to Check it!')
