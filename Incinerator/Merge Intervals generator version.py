3b8e2bbfd4db9e99801e957ece78e703989f0f57
mal1on <lyubo.malinov@gmail.com>
2024-01-18 12:17:47 +0200

    Merge Intervals (generator version) - mission completed

--- Incinerator/Merge Intervals generator version.py
+++ Incinerator/Merge Intervals generator version.py
@@ -15,6 +15,17 @@ def merge_intervals(intervals):
         yield i
 
 
-print(list(merge_intervals([(1, 4), (2, 6), (8, 10), (12, 19)]))) == [(1, 6), (8, 10), (12, 19)]
-list(merge_intervals([(1, 12), (2, 3), (4, 7)])) == [(1, 12)]
-list(merge_intervals([(1, 5), (6, 10), (10, 15), (17, 20)])) == [(1, 15), (17, 20)]
+if __name__ == "__main__":
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert list(merge_intervals([(1, 4), (2, 6), (8, 10), (12, 19)])) == [
+        (1, 6),
+        (8, 10),
+        (12, 19),
+    ], "First"
+    assert list(merge_intervals([(1, 12), (2, 3), (4, 7)])) == [
+        (1, 12)], "Second"
+    assert list(merge_intervals([(1, 5), (6, 10), (10, 15), (17, 20)])) == [
+        (1, 15),
+        (17, 20),
+    ], "Third"
+    print("Done! Go ahead and Check IT")
