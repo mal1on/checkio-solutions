d47e8907366156785916f87d339e0f39441d3f3e
mal1on <lyubo.malinov@gmail.com>
2024-01-27 01:00:32 +0200

    Initiation - Count Substring Occurrences - mission completed

--- Initiation/Count Substring Occurrences.py
+++ Initiation/Count Substring Occurrences.py
@@ -1,10 +1,33 @@
 def count_occurrences(main_str: str, sub_str: str) -> int:
-    # your code here
-    return 0
+
+    result = start = 0
+    main_str = main_str.lower()
+    sub_str = sub_str.lower()
+
+    while start <= (len(main_str) - len(sub_str)):
+        if main_str.find(sub_str, start) != -1:
+            result += 1
+            start += main_str.find(sub_str, start) + 1
+        else:
+            break
+
+    return result
 
 
+print("Example:")
+print(count_occurrences("hello world hello", "hello"))
 
-count_occurrences("hello world hello", "hello") == 2
-count_occurrences("Hello World hello", "hello") == 2
-count_occurrences("hello", "world") == 0
-count_occurrences("hello world hello world hello", "world") == 2
+# These "asserts" are used for self-checking
+assert count_occurrences("hello world hello", "hello") == 2
+assert count_occurrences("Hello World hello", "hello") == 2
+assert count_occurrences("hello", "world") == 0
+assert count_occurrences("hello world hello world hello", "world") == 2
+assert count_occurrences("HELLO", "hello") == 1
+assert count_occurrences("appleappleapple", "appleapple") == 2
+assert count_occurrences("HELLO WORLD", "WORLD") == 1
+assert count_occurrences("hello world hello", "o w") == 1
+assert count_occurrences("apple apple apple", "apple") == 3
+assert count_occurrences("apple Apple apple", "apple") == 3
+assert count_occurrences("apple", "APPLE") == 1
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
