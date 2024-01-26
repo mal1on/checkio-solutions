58a642061543d82d8a1363cc65253d80616c0a30
mal1on <lyubo.malinov@gmail.com>
2024-01-26 22:45:52 +0200

    Initiation - Count Substring Occurrences - mission file created

---
+++ Initiation/Count Substring Occurrences.py
@@ -0,0 +1,10 @@
+def count_occurrences(main_str: str, sub_str: str) -> int:
+    # your code here
+    return 0
+
+
+
+count_occurrences("hello world hello", "hello") == 2
+count_occurrences("Hello World hello", "hello") == 2
+count_occurrences("hello", "world") == 0
+count_occurrences("hello world hello world hello", "world") == 2
