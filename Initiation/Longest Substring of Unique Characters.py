189e3f30753d88f45b72350761a9b6b24311d087
mal1on <lyubo.malinov@gmail.com>
2024-01-28 22:11:25 +0200

    Longest Substring of Unique Characters - mission file created

---
+++ Initiation/Longest Substring of Unique Characters.py
@@ -0,0 +1,10 @@
+def longest_substr(s: str) -> int:
+    # your code here
+    return 0
+
+
+
+longest_substr("abcabcbb") == 3
+longest_substr("bbbbb") == 1
+longest_substr("pwwkew") == 3
+longest_substr("abcdef") == 6
