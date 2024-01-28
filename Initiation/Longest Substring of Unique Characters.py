c5bc072c9ffd03624394c9cb836725fe31e07238
mal1on <lyubo.malinov@gmail.com>
2024-01-28 22:23:59 +0200

    Longest Substring of Unique Characters - mission completed

--- Initiation/Longest Substring of Unique Characters.py
+++ Initiation/Longest Substring of Unique Characters.py
@@ -1,10 +1,29 @@
 def longest_substr(s: str) -> int:
-    # your code here
-    return 0
+
+    temp = ''
+    strings = ['']
+
+    for i in range(len(s)):
+        for ch in s[i:]:
+            if ch not in temp:
+                temp += ch
+            else:
+                strings.append(temp)
+                temp = ch
+
+    return len(max(strings, key=len))
 
 
+print("Example:")
+print(longest_substr("abcabcbb"))
 
-longest_substr("abcabcbb") == 3
-longest_substr("bbbbb") == 1
-longest_substr("pwwkew") == 3
-longest_substr("abcdef") == 6
+# These "asserts" are used for self-checking
+assert longest_substr("abcabcbb") == 3
+assert longest_substr("bbbbb") == 1
+assert longest_substr("pwwkew") == 3
+assert longest_substr("abcdef") == 6
+assert longest_substr("") == 0
+assert longest_substr("au") == 2
+assert longest_substr("dvdf") == 3
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
