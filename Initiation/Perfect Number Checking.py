4be4c34979f7933f474871bf30cc21c05199cad2
mal1on <lyubo.malinov@gmail.com>
2024-01-30 22:44:00 +0200

    Perfect Number Checking - mission completed

--- Initiation/Perfect Number Checking.py
+++ Initiation/Perfect Number Checking.py
@@ -1,10 +1,21 @@
 def is_perfect(n: int) -> bool:
-    # your code here
-    return False
+
+    return sum([d for d in range(1, n) if n % d == 0]) == n
 
 
+print("Example:")
+print(is_perfect(3))
 
-is_perfect(6) == True
-is_perfect(2) == False
-is_perfect(28) == True
-is_perfect(20) == False
+# These "asserts" are used for self-checking
+assert is_perfect(6) == True
+assert is_perfect(2) == False
+assert is_perfect(28) == True
+assert is_perfect(20) == False
+assert is_perfect(496) == True
+assert is_perfect(30) == False
+assert is_perfect(8128) == True
+assert is_perfect(100) == False
+assert is_perfect(500) == False
+assert is_perfect(1000) == False
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
