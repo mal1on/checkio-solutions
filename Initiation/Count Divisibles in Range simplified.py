e1541f35fe89067480dc5ca719ecdece172111dd
mal1on <lyubo.malinov@gmail.com>
2024-01-31 22:16:20 +0200

    Count Divisibles in Range (simplified) - mission file completed

--- Initiation/Count Divisibles in Range simplified.py
+++ Initiation/Count Divisibles in Range simplified.py
@@ -1,10 +1,21 @@
 def count_divisible(n: int, k: int) -> int:
-    # your code here
-    return 0
+
+    return n // k
 
 
+print("Example:")
+print(count_divisible(2, 1))
 
-count_divisible(10, 2) == 5
-# count_divisible(10, 3) == 3
-# count_divisible(10, 5) == 2
-# count_divisible(15, 4) == 3
+# These "asserts" are used for self-checking
+assert count_divisible(10, 2) == 5
+assert count_divisible(10, 3) == 3
+assert count_divisible(10, 5) == 2
+assert count_divisible(15, 4) == 3
+assert count_divisible(20, 6) == 3
+assert count_divisible(100, 10) == 10
+assert count_divisible(200, 25) == 8
+assert count_divisible(50, 7) == 7
+assert count_divisible(60, 8) == 7
+assert count_divisible(70, 9) == 7
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
