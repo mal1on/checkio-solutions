da3f975676aca295c7fc6a8359bae571a5193d1f
mal1on <lyubo.malinov@gmail.com>
2024-01-31 21:42:21 +0200

    Count Divisibles in Range (simplified) - mission file created

---
+++ Initiation/Count Divisibles in Range simplified.py
@@ -0,0 +1,10 @@
+def count_divisible(n: int, k: int) -> int:
+    # your code here
+    return 0
+
+
+
+count_divisible(10, 2) == 5
+# count_divisible(10, 3) == 3
+# count_divisible(10, 5) == 2
+# count_divisible(15, 4) == 3
