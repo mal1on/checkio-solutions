97d644e2c02e12274e2d632f59b654a8ef013c07
mal1on <lyubo.malinov@gmail.com>
2024-08-16 14:36:16 +0300

    Fyzzy String Matching - mission completed

--- Initiation/Fuzzy String Matching.py
+++ Initiation/Fuzzy String Matching.py
@@ -1,11 +1,21 @@
 from itertools import zip_longest
 
+
 def fuzzy_string_match(str1: str, str2: str, threshold: int) -> bool:
 
-    print(len([x for x in map(set, zip_longest(str1, str2)) if len(x) > 1]) <= threshold)
+    return len([x for x in map(set, zip_longest(str1, str2)) if len(x) > 1]) <= threshold
 
 
-fuzzy_string_match("apple", "appel", 2)
-fuzzy_string_match("apple", "bpple", 1)
-fuzzy_string_match("apple", "bpple", 0)
-fuzzy_string_match("apple", "apples", 1)
+print("Example:")
+print(fuzzy_string_match("apple", "appel", 2))
+
+# These "asserts" are used for self-checking
+assert fuzzy_string_match("apple", "appel", 2) == True
+assert fuzzy_string_match("apple", "bpple", 1) == True
+assert fuzzy_string_match("apple", "bpple", 0) == False
+assert fuzzy_string_match("apple", "apples", 1) == True
+assert fuzzy_string_match("apple", "bpples", 2) == True
+assert fuzzy_string_match("apple", "apxle", 1) == True
+assert fuzzy_string_match("apple", "pxxli", 3) == False
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
