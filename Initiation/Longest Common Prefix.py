1fb3f0100f68c7d2f40b628a9b1dde4525b6a9f9
mal1on <lyubo.malinov@gmail.com>
2024-08-16 20:16:06 +0300

    Longest Common Prefix - all checks successful

--- Initiation/Longest Common Prefix.py
+++ Initiation/Longest Common Prefix.py
@@ -1,8 +1,16 @@
+from itertools import zip_longest
+
 def longest_prefix(arr: list[str]) -> str:
-    # your code here
-    return ""
 
+    prefix = ''
 
+    for ch in map(list, map(set, zip_longest(*arr))):
+        if len(ch) == 1:
+            prefix += ch[0]
+        else:
+            break
+
+    print(prefix)
 
 longest_prefix(["flower", "flow", "flight"])
 longest_prefix(["dog", "racecar", "car"])
