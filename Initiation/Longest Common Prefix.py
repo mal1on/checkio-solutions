c9f548a57be282176efb66cccacbd7d1617ce357
mal1on <lyubo.malinov@gmail.com>
2024-08-16 19:56:59 +0300

    Longest Common Prefix - mission file created

---
+++ Initiation/Longest Common Prefix.py
@@ -0,0 +1,10 @@
+def longest_prefix(arr: list[str]) -> str:
+    # your code here
+    return ""
+
+
+
+longest_prefix(["flower", "flow", "flight"])
+longest_prefix(["dog", "racecar", "car"])
+longest_prefix(["apple", "application", "appetizer"])
+longest_prefix(["a"])
