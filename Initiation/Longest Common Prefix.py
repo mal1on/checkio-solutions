8410ab0c2da3bc3b6ccf60232e9baaf6509aa77f
mal1on <lyubo.malinov@gmail.com>
2024-08-16 20:19:18 +0300

    Longest Common Prefix - mission completed

--- Initiation/Longest Common Prefix.py
+++ Initiation/Longest Common Prefix.py
@@ -1,5 +1,6 @@
 from itertools import zip_longest
 
+
 def longest_prefix(arr: list[str]) -> str:
 
     prefix = ''
@@ -10,9 +11,22 @@ def longest_prefix(arr: list[str]) -> str:
         else:
             break
 
-    print(prefix)
+    return prefix
 
-longest_prefix(["flower", "flow", "flight"])
-longest_prefix(["dog", "racecar", "car"])
-longest_prefix(["apple", "application", "appetizer"])
-longest_prefix(["a"])
+
+print("Example:")
+print(repr(longest_prefix(["flower", "flow", "flight"])))
+
+# These "asserts" are used for self-checking
+assert longest_prefix(["flower", "flow", "flight"]) == "fl"
+assert longest_prefix(["dog", "racecar", "car"]) == ""
+assert longest_prefix(["apple", "application", "appetizer"]) == "app"
+assert longest_prefix(["a"]) == "a"
+assert longest_prefix(["", "b"]) == ""
+assert longest_prefix(["same", "same", "same"]) == "same"
+assert longest_prefix(["mismatch", "mister", "miss"]) == "mis"
+assert longest_prefix(["alphabet", "alpha", "alphadog"]) == "alpha"
+assert longest_prefix(["book", "boot", "booster"]) == "boo"
+assert longest_prefix(["short", "shore", "shot"]) == "sho"
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
