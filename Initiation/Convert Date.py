0c2add157bfbf4969eb7653e85ecf1a8f51d2c22
mal1on <lyubo.malinov@gmail.com>
2024-09-04 17:34:45 +0300

    Convert Date - mission file created

---
+++ Initiation/Convert Date.py
@@ -0,0 +1,10 @@
+def convert_date(date: str) -> str:
+    # your code here
+    return ""
+
+
+
+convert_date("25/12/2021")
+convert_date("01/01/2000")
+convert_date("15/06/1995")
+convert_date("29/02/2020")
