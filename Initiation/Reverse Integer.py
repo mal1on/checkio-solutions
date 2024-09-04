38513ac77da6a651ac446c0796142d351b1813d6
mal1on <lyubo.malinov@gmail.com>
2024-09-04 11:53:22 +0300

    Reverse Integer - mission completed

--- Initiation/Reverse Integer.py
+++ Initiation/Reverse Integer.py
@@ -1,10 +1,21 @@
 def reverse_digits(num: int) -> int:
-    # your code here
-    return 0
+
+    return int('-' + str(num)[::-1][:-1] if str(num)[::-1].endswith('-') else str(num)[::-1])
 
 
+print("Example:")
+print(reverse_digits(32))
 
-reverse_digits(1234)
-reverse_digits(0)
-reverse_digits(-123)
-reverse_digits(5)
+# These "asserts" are used for self-checking
+assert reverse_digits(1234) == 4321
+assert reverse_digits(0) == 0
+assert reverse_digits(-123) == -321
+assert reverse_digits(5) == 5
+assert reverse_digits(-9) == -9
+assert reverse_digits(100) == 1
+assert reverse_digits(-100) == -1
+assert reverse_digits(54321) == 12345
+assert reverse_digits(1111) == 1111
+assert reverse_digits(987654321) == 123456789
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
