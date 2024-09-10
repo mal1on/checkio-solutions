d5728088854b5ad9e2cc58414bd3ddd968bf250a
mal1on <lyubo.malinov@gmail.com>
2024-09-10 14:17:20 +0300

    Quadratic Equation Roots - mission file created

---
+++ Initiation/Quadratic Equation Roots.py
@@ -0,0 +1,10 @@
+def quadratic_roots(a: int, b: int, c: int) -> Iterable[Union[int | float] | str]:
+
+    return []
+
+
+
+
+print(quadratic_roots(1, -3, 2))
+print(quadratic_roots(1, 0, -1))
+print(quadratic_roots(1, 0, 1))
