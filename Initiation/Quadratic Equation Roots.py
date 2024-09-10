07a7f7a1a4362ad66566979080d0185ea1d33294
mal1on <lyubo.malinov@gmail.com>
2024-09-10 18:20:35 +0300

    Quadratic Equation Roots - all checks successful

--- Initiation/Quadratic Equation Roots.py
+++ Initiation/Quadratic Equation Roots.py
@@ -1,10 +1,18 @@
-def quadratic_roots(a: int, b: int, c: int) -> Iterable[Union[int | float] | str]:
+def quadratic_roots(a, b, c):
 
-    return []
+    d = b*b - 4*a*c
+    sd = pow(d, 1/2)
+
+    if d > 0:
+        return [int((-b+sd)/2*a), int((-b-sd)/2*a)]
+    elif d == 0:
+        return [int((-b+sd)//2*a), int((-b+sd)/2*a)]
+    return ["No real roots"]
 
 
 
 
 print(quadratic_roots(1, -3, 2))
 print(quadratic_roots(1, 0, -1))
+print(quadratic_roots(1, 2, 1))
 print(quadratic_roots(1, 0, 1))
