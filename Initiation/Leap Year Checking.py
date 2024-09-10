ee146f8254b3f04950ff77cc1663e49c2707a570
mal1on <lyubo.malinov@gmail.com>
2024-09-10 11:48:56 +0300

    Leap Year Checking - mission completed

--- Initiation/Leap Year Checking.py
+++ Initiation/Leap Year Checking.py
@@ -7,10 +7,19 @@ def is_leap_year(year: int) -> bool:
     return False
 
 
+print("Example:")
+print(is_leap_year(1891))
 
+# These "asserts" are used for self-checking
+assert is_leap_year(2000) == True
+assert is_leap_year(1900) == False
+assert is_leap_year(2004) == True
+assert is_leap_year(2100) == False
+assert is_leap_year(2020) == True
+assert is_leap_year(2021) == False
+assert is_leap_year(1600) == True
+assert is_leap_year(1700) == False
+assert is_leap_year(1800) == False
+assert is_leap_year(2400) == True
 
-
-print(is_leap_year(2000))
-print(is_leap_year(1900))
-print(is_leap_year(2004))
-print(is_leap_year(2100))
+print("The mission is done! Click 'Check Solution' to earn rewards!")
